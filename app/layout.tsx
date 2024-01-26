d0c869f4c99a1eb5e34d1a76f33d1cff80534043
hadiRazal <hadhirasal22@gmail.com>
2024-01-26 23:59:58 +0530

    added the toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
@@ -22,6 +23,10 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Header />
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+        />
         {children}
         <Footer />
       </body>
