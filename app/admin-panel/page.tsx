98aa096bdf09e23a9933d74f94f275daa40c3cf3
hadiRazal <hadhirasal22@gmail.com>
2024-01-26 23:29:45 +0530

    added color adding option in product page

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -5,7 +5,7 @@ import { IoMdArrowBack } from "react-icons/io";
 import { CiSquarePlus } from "react-icons/ci";
 import Image from "next/image";
 import { useState } from "react";
-
+import { MdDelete } from "react-icons/md";
 
 
 
@@ -14,7 +14,15 @@ const AddProduct = () => {
 
   const [coverImage, setCoverImage] = useState<string | null>(null);
   const [moreImages, setMoreImages] = useState<string[]>([]);
+  const [colorInput, setColorInput] = useState<string | null>(null);
+  const [colorCode, setColorCode] = useState<string[]>([]);
 
+  const handleAddColors = () => {
+    if (colorInput) {
+      setColorCode((prev) => [...prev, colorInput]);
+      setColorInput(""); // Clear the input field after adding the color
+    }
+  };
   const handleCoverImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -37,6 +45,8 @@ const AddProduct = () => {
 
   console.log(coverImage);
   console.log(moreImages);
+  console.log(colorCode)
+  console.log(colorInput)
 
   return (
     <div className='flex flex-col gap-3 py-5 md:px-10 px-5'>
@@ -44,6 +54,8 @@ const AddProduct = () => {
         <IoMdArrowBack className="cursor-pointer hover:scale-110" onClick={() => router.back()} size={24} />
         <h1 className='font-bold text-[30px]'>Add new Product</h1>
       </div>
+
+
       {/* Product Details */}
       <div className='flex flex-col w-full gap-4'>
         <h1 className='text-[20px] font-bold'>Product Details</h1>
@@ -68,14 +80,14 @@ const AddProduct = () => {
         <div className='flex flex-col gap-2'>
           <h1 className=" font-bold">Description</h1>
           <textarea rows={10} className='bg-gray-200 rounded-2xl px-5 py-3' minLength={100} name="desc" id="desc" >
-            fsef
           </textarea>
         </div>
 
+
         {/* Sizes */}
-        <div className=' gap-3'>
+        <div className=' gap-3 flex flex-col '>
           <h1 className=" font-bold">Available Sizes</h1>
-          <div className='flex gap-3'>
+          <div className='flex gap-3 flex-wrap'>
             <input type="checkbox" />
             <span>Small</span>
             <input type="checkbox" />
@@ -89,6 +101,7 @@ const AddProduct = () => {
           </div>
         </div>
       </div>
+
 
 
       {/* Pricing */}
@@ -107,18 +120,35 @@ const AddProduct = () => {
         </div>
       </div>
 
+
+      {/* Available color */}
       <div className="flex flex-col gap-3">
-        <h1 className=" font-bold">Available Colors</h1>
+        <h1 className="font-bold">Available Colors</h1>
         <label htmlFor="tile">Add Color Code</label>
         <div className="flex gap-3">
-          <input type="text" name='title' className='bg-gray-200 rounded-2xl px-5 py-3' />
-          <button className="bg-td-primary rounded-2xl px-3 py-3 text-white">Add</button>
+          <input
+            type="text"
+            name="title"
+            value={colorInput || ""}
+            className="bg-gray-200 rounded-2xl px-5 py-3"
+            onChange={(e) => setColorInput(e.target.value)}
+          />
+          <button
+            className="bg-td-primary rounded-2xl px-3 py-3 text-white"
+            onClick={handleAddColors}
+          >
+            Add
+          </button>
         </div>
         <div className="flex gap-3 items-center justify-start">
-          <h1>Selected Colors : </h1>
-          <span className='h-[25px] bg-[#421f4a] w-[25px] rounded-[50%] inline-block'></span>
-          <span className='h-[25px] bg-[#3f9693] w-[25px] rounded-[50%] inline-block'></span>
-          <span className='h-[25px] bg-[#783246] w-[25px] rounded-[50%] inline-block'></span>
+          <h1>Selected Colors :</h1>
+          {colorCode.map((color, index) => (
+            <span
+              key={index}
+              style={{ backgroundColor: color }}
+              className={`h-[50px] cursor-pointer w-[50px] rounded-[50%] inline-block`}
+            ></span>
+          ))}
         </div>
       </div>
 
@@ -126,21 +156,23 @@ const AddProduct = () => {
 
       {/* Images */}
 
-      <div className="flex flex-col w-full gap-3">
+      <div className="flex  flex-col w-full gap-3">
         <h1 className=" font-bold">Images</h1>
         <div className="flex flex-col gap-2 w-full">
           <span className="font-semibold">Cover Image</span>
-          <div className="flex  px-5 flex-col items-center justify-center gap-3 md:w-[400px]  w-full h-[200px]">
+          <div className="flex px-5 items-center justify-center gap-3  w-full  ">
             {!coverImage && (
-              <label htmlFor="coverImage" className="w-full flex flex-col items-center justify-center">
+              <label htmlFor="coverImage" className="w-[290px] rounded-2xl border flex flex-col items-center justify-center h-[290px]">
                 <span className="font-bold">Add Image</span>
                 <CiSquarePlus size={24} />
               </label>
             )}
             {coverImage && (
-              <div className="flex flex-col items-center justify-center gap-2 py-5 w-full">
-                <img src={coverImage} alt="Cover" className="object-cover" height={150} width={150} />
-                <button onClick={() => setCoverImage(null)} className="bg-red-700 px-3 py-2 rounded-2xl text-white">Remove</button>
+              <div className="w-full flex flex-col items-center justify-center gap-2">
+                <Image src={coverImage} alt="Cover" className="w-[290px] h-[290px] object-cover rounded-2xl" height={150} width={150} />
+                <button onClick={() => setCoverImage(null)} className="bg-red-700 px-3 py-2 rounded-2xl text-white">
+                  <MdDelete size={24} />
+                </button>
               </div>
             )}
             <input
@@ -158,22 +190,26 @@ const AddProduct = () => {
 
         <div className="w-full">
           <span className="font-semibold">More Images</span>
-          <div className="flex px-5 items-center justify-center gap-3 md:w-[400px] w-full h-[200px]">
-            {moreImages.length < 3 ? (
-              <>
-                <label htmlFor="moreImages" className="w-full h-full flex flex-col items-center justify-center">
+          <div className="flex flex-col px-5 items-center justify-center gap-3 md:w-full w-full ">
+            {moreImages.length < 4 ? (
+              <div className="w-full flex items-center justify-center">
+                <label htmlFor="moreImages" className="w-[290px] rounded-2xl border flex flex-col items-center justify-center h-[290px]">
                   <span className="font-bold">Add Image</span>
                   <CiSquarePlus size={24} />
                 </label>
                 <input type="file" className="hidden" id="moreImages" accept="image/*" onChange={handleMoreImageChange} />
-              </>
-            ) : null}
-            {moreImages.map((imageUrl, index) => (
-              <div key={index} className="w-full flex flex-col items-center justify-center gap-2">
-                <img src={imageUrl} alt={`More Image ${index + 1}`} className="w-full h-full object-cover" />
-                <button onClick={() => unSelectImage(index)} className="bg-red-700 px-3 py-2 rounded-2xl text-white">Remove</button>
               </div>
-            ))}
+            ) : null}
+            <div className="flex md:flex-row flex-col gap-4 py-5">
+              {moreImages.map((imageUrl, index) => (
+                <div key={index} className="w-full flex flex-col items-center justify-center gap-2">
+                  <Image width={50} height={50} src={imageUrl} alt={`More Image ${index + 1} `} className="w-[290px] h-[290px] object-cover rounded-2xl" />
+                  <button onClick={() => unSelectImage(index)} className="bg-red-700 px-3 py-2 rounded-2xl text-white">
+                    <MdDelete size={24} />
+                  </button>
+                </div>
+              ))}
+            </div>
 
           </div>
         </div>
