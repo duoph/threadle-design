b12bbae827eadfa4525eb4a090c6e85f6be0ffbe
hadiRazal <hadhirasal22@gmail.com>
2024-01-27 02:07:24 +0530

    footer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-/* eslint-disable react/no-unescaped-entities */ 
+/* eslint-disable react/no-unescaped-entities */
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -8,12 +8,11 @@ import React from 'react'
 
 import { FaFacebook, FaInstagram } from 'react-icons/fa6'
 
-
 const Footer = () => {
     return (
         <div className='py-5 flex flex-col gap-3  bg-slate-200 w-full md:px-20 px-5'>
             <div className='flex md:flex-row flex-col justify-between items-center gap-4 '>
-                <div className='flex flex-col  md:w-1/2 w-full gap-3'>
+                <div className='flex flex-col md:items-start items-center md:w-1/2 w-full gap-3'>
                     <Image src={'/td-green.png'} alt='Logo' width={100} height={100} />
                     <p className='break-all'>We have clothes that suits your style and which you're proud to wear.</p>
                     <span className='flex gap-3'>
@@ -21,7 +20,7 @@ const Footer = () => {
                         <FaInstagram className='cursor-pointer' size={24} />
                     </span>
                 </div>
-                <div className='flex flex-col items-end md:w-1/2 w-full gap-3'>
+                <div className='flex flex-col md:items-end items-center md:w-1/2 w-full gap-3'>
                     <span className='font-semibold text-[20px] underline'>Quick Links</span>
                     <Link href={'/about'}>About</Link>
                     <Link href={'/terms-condition'}>Terms & Conditions</Link>
