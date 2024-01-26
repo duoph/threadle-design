b85fab1378c002b5b28786bf3b8016f1a12d014a
hadiRazal <hadhirasal22@gmail.com>
2024-01-27 01:32:41 +0530

    made changes in footer

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-const page = () => { 
+const page = () => {
   return (
-    <div>login</div>
+    <div className='h-[50vh] flex items-center justify-center '>
+      <Link href="/admin-panel" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Go To The Admin Pannel</Link>
+    </div>
   )
 }
 
