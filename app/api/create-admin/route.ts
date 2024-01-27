35243345f5911c9af29031cd8b44bef6ead17c81
hadiRazal <hadhirasal22@gmail.com>
2024-01-28 02:27:40 +0530

    added create admin route

--- app/api/create-admin/route.ts
+++ app/api/create-admin/route.ts
@@ -1,29 +1,32 @@
 import connectMongoDB from "@/libs/db";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import AdminModel from "@/models/adminModel";
 
 
 export async function POST(req: NextRequest) {
     try {
+        connectMongoDB()
         const { email, password, } = await req.json()
-
         if (!email || !password) {
             return NextResponse.json({ message: "Enter valid credentials", success: false })
         }
 
         // check email already exisiting or not
+        const emailExists = await AdminModel.findOne({ email });
+        if (emailExists) {
+            return NextResponse.json({ message: "Email already exists", success: false });
+        }
 
         // hash password
         const hashedPassword = await bcrypt.hash(password, 10)
+        await AdminModel.create({
+            email, password: hashedPassword
+        })
+        return NextResponse.json({ message: "Admin account created succesfully", success: true })
 
-        const createUser =
-
-       console.log(email, hashedPassword)
-        connectMongoDB()
-        return NextResponse.json({ hashedPassword, email, success: true })
     } catch (error) {
-        return NextResponse.json({ message: "Error in creating a new admin", success: false })
+        return NextResponse.json({ message: "Error in creating a new admin", success: false, error })
     }
-
 }
 
