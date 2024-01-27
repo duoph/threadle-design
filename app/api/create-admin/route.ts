9456c73dada6cf3ba035c35e2945434f4dc67c29
hadiRazal <hadhirasal22@gmail.com>
2024-01-28 01:45:38 +0530

    create admin schema

--- app/api/create-admin/route.ts
+++ app/api/create-admin/route.ts
@@ -7,16 +7,20 @@ export async function POST(req: NextRequest) {
     try {
         const { email, password, } = await req.json()
 
-        // check email already exisiting or not
+        if (!email || !password) {
+            return NextResponse.json({ message: "Enter valid credentials", success: false })
+        }
 
+        // check email already exisiting or not
 
         // hash password
         const hashedPassword = await bcrypt.hash(password, 10)
 
+        const createUser =
 
-        console.log(email, hashedPassword)
+       console.log(email, hashedPassword)
         connectMongoDB()
-        return NextResponse.json({ hashedPassword, email })
+        return NextResponse.json({ hashedPassword, email, success: true })
     } catch (error) {
         return NextResponse.json({ message: "Error in creating a new admin", success: false })
     }
