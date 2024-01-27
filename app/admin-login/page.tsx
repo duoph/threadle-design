7eb7f883bdf1f4a0feba08b4f8b11e313f5e1656
hadiRazal <hadhirasal22@gmail.com>
2024-01-27 12:45:38 +0530

    added login page

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -1,12 +1,37 @@
-import Link from 'next/link'
-import React from 'react'
 
-const page = () => {
+"use client"
+
+import React, { useState } from 'react'
+
+const LoginPageAdmin = () => {
+
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+
+
+  const handleLogin = () => {
+    try {
+
+    } catch (error) {
+
+    }
+  }
+
+  console.log(password)
+  console.log(email)
+
   return (
-    <div className='h-[50vh] flex items-center justify-center '>
-      <Link href="/admin-panel" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Go To The Admin Pannel</Link>
+    <div className='flex flex-col gap-10 items-center justify-center w-full md:px-10 px-5 py-[200px]'>
+      <div>
+        <h1 className='font-bold text-[30px] text-td-secondary'>Login As Admin</h1>
+      </div>
+      <div className='flex flex-col gap-3 md:w-[400px] w-full'>
+        <input type="text" placeholder='Email' className='border px-5 py-2 rounded-2xl bg-slate-200' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='Password' className='border px-5 py-2 rounded-2xl bg-slate-200' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleLogin} className='bg-td-secondary rounded-2xl px-3 py-3 text-white font-semibold hover:bg-td-primary'>Login</button>
+      </div>
     </div>
   )
 }
 
-export default page
+export default LoginPageAdmin
