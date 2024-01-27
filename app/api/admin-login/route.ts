5cf827c2fa390f89a7f4db42ddb4c82d60d28593
hadiRazal <hadhirasal22@gmail.com>
2024-01-28 02:56:00 +0530

    added admin login

---
+++ app/api/admin-login/route.ts
@@ -0,0 +1,38 @@
+import connectMongoDB from "@/libs/db";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt"
+import JWT from "jsonwebtoken";
+import AdminModel from "@/models/adminModel";
+
+
+export async function POST(req: NextRequest) {
+    try {
+        connectMongoDB()
+        const { email, password, } = await req.json()
+        if (!email || !password) {
+            return NextResponse.json({ message: "Enter valid credentials", success: false })
+        }
+
+        // check email already exisiting or not
+        const admin = await AdminModel.findOne({ email });
+        if (!admin) {
+            return NextResponse.json({ message: "Check your email or password", success: false });
+        }
+
+        // compare password
+        const passCompare = await bcrypt.compare(password, admin.password)
+
+        if (!passCompare) {
+            return NextResponse.json({ message: "Check your password", success: false })
+        }
+
+        // Generate JWT token
+        const token = JWT.sign({ _id: admin._id }, process.env.NEXT_PUBLIC_JWT_SECRET as string, { expiresIn: '7d' });
+
+        return NextResponse.json({ message: "Logged in succesfully", success: true, admin, token })
+
+    } catch (error) {
+        return NextResponse.json({ message: "Error in logging as an admin", success: false, error })
+    }
+}
+
