1f6cd3d589f06f86d91fe2f26dff0324e134b91c
hadiRazal <hadhirasal22@gmail.com>
2024-01-30 11:02:36 +0530

    test error

--- app/api/admin-panel/category/route.ts
+++ app/api/admin-panel/category/route.ts
@@ -8,30 +8,43 @@ import slugify from "slugify";
 
 export async function POST(request: NextRequest) {
     try {
+        console.error("1");
         connectMongoDB();
         const formData = await request.formData();
         const title = formData.get("title");
         const file = formData.get("file");
 
+
+        console.error("2");
+  
+
+
         if (!file || !(file instanceof File)) {
             throw new Error("File is missing or invalid");
         }
+        console.error("3");
 
         const fileBuffer = await file.arrayBuffer();
         const buffer = Buffer.from(fileBuffer);
+        console.error("4");
+
 
         const slugifyCategoryName = slugify(title as string, { lower: true });
 
         const aws = await uploadFileToS3(buffer, title as string);
+        console.error("5");
+
 
         console.log(aws);
+        console.error("6");
 
-        // Ensure the uploadFileToS3 operation has completed before proceeding
         await CategoryModel.create({
             categoryName: title,
             slugifyName: slugifyCategoryName,
             imageURL: aws?.s3Url
         });
+        console.error("7");
+
 
         console.log("Category created successfully");
 
