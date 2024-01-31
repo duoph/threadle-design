b69df72a3a619345f7680b630f9b0e48e1e70e09
hadiRazal <hadhirasal22@gmail.com>
2024-01-31 19:44:22 +0530

    added admin creation

--- app/api/admin-panel/create-admin/route.ts
+++ app/api/admin-panel/create-admin/route.ts
@@ -1,20 +1,35 @@
 import connectMongoDB from "@/libs/db";
+import userModel from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
-
-
+import { findDOMNode } from "react-dom";
 
 export async function POST(req: NextRequest) {
     try {
-        connectMongoDB()
+        await connectMongoDB(); // Make sure to await the connection
 
-        const { email } = req.json()
+        const { email } = await req.json(); // Make sure to await the body parsing
 
-        return NextResponse.json({
-            message: `This is the email you sented to us,:${email}`
+
+        const checkEmailisAlreadyAdmin = await userModel.findOne({ email })
+
+        if (checkEmailisAlreadyAdmin.isAdmin === true) {
+            return NextResponse.json({
+                message: `${email} is already an admin`, success: true
+            });
+        }
+
+        await userModel.findOneAndUpdate({
+            email, isAdmin: true
         })
 
+        return NextResponse.json({
+            message: `${email} is admin now`, success: true
+        });
+
     } catch (error) {
-        console.log(error)
+        console.error(error); // Use console.error for logging errors
+        return NextResponse.json({
+            message: "An error occurred while processing your request.", success: false
+        });
     }
 }
-
