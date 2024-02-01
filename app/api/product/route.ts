0665999a3dbe888bbd3ba4d7cbd2dfee06d8f306
hadiRazal <hadhirasal22@gmail.com>
2024-02-01 22:53:59 +0530

    product route error

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,8 +1,9 @@
 import { uploadFileToS3 } from "@/actions/awsS3Upload";
 import ProductModel from "@/models/productModel";
+import { TIMEOUT } from "dns";
+import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 import slugify from "slugify";
-
 
 export async function POST(req: NextRequest) {
     try {
@@ -19,8 +20,14 @@ export async function POST(req: NextRequest) {
         const image4 = formData.get("image4") as File;
         const salePrice = formData.get("salePrice") || undefined;
 
+        console.log(title, desc, regularPrice);
+
         if (!coverImage || !(coverImage instanceof File)) {
             throw new Error("Cover image is missing or invalid");
+        }
+
+        if (!title || !desc || !category || !coverImage) {
+            throw new Error("Missing required fields");
         }
 
         const coverImageBuffer = await coverImage.arrayBuffer();
@@ -28,32 +35,51 @@ export async function POST(req: NextRequest) {
 
         const slugifyProductName = slugify(title as string, { lower: true });
 
-        // Upload cover image to S3
         const coverImageURL = await uploadFileToS3(coverImageBufferArray, slugifyProductName);
 
-        // Upload additional images to S3
+
+
         const imageUrls = await Promise.all([
             uploadFileToS3(Buffer.from(await image1.arrayBuffer()), slugifyProductName + "-image1"),
             uploadFileToS3(Buffer.from(await image2.arrayBuffer()), slugifyProductName + "-image2"),
             uploadFileToS3(Buffer.from(await image3.arrayBuffer()), slugifyProductName + "-image3"),
             uploadFileToS3(Buffer.from(await image4.arrayBuffer()), slugifyProductName + "-image4"),
         ]);
-        // Create a new product document
-        const newProduct = await ProductModel.create({
-            title,
-            desc,
-            regularPrice,
+
+        console.log(title, desc, regularPrice, coverImageURL, imageUrls, slugifyProductName);
+
+        await ProductModel.create({
+            title: title,
+            desc: desc,
+            regularPrice: regularPrice,
             coverImageURL: coverImageURL?.s3Url,
-            salePrice,
-            category,
-            slugifyProductName,
+            salePrice: salePrice,
+            category: category,
+            slugifyProductName: slugifyProductName,
             inStock: true,
             moreImagesURLs: imageUrls.map((result: any) => result.s3Url),
-        });
+        }, { TIMEOUT: 100000 });
 
-        return NextResponse.json(newProduct.toJSON()); // You can customize the response as needed
+        return NextResponse.json({ message: "Product created successfully", success: true });
     } catch (error) {
         console.error(error);
         return NextResponse.json({ message: "Unknown error", error });
     }
+}
+
+
+
+// get all products from db
+
+export async function GET() {
+
+    try {
+        const allProducts = await ProductModel.find({})
+        return NextResponse.json({ allProducts, success: true })
+
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(error)
+    }
+
 }
