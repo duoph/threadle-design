d5ce0963842cc124c08d482e294fd711e65ad3e9
hadiRazal <hadhirasal22@gmail.com>
2024-02-02 23:46:22 +0530

    test

---
+++ app/api/product/[categoryId]/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest } from "next/server";
+
+
+export async function GET(req: NextRequest) {
+    try {
+        const categoryId = req.headers
+    } catch (error) {
+
+    }
+
+}
