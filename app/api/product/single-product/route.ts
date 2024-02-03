62ecd22137cd6199a411ad49547cd25ec0418b6c
hadiRazal <hadhirasal22@gmail.com>
2024-02-03 11:57:04 +0530

    added category delete

---
+++ app/api/product/single-product/route.ts
@@ -0,0 +1,12 @@
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+    try {
+        const url = req.nextUrl.searchParams.get('cat')
+        console.log(url)
+        return NextResponse.json("test")
+    } catch (error) {
+        console.log(error)
+    }
+}
