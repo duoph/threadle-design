7f5a9a3883658bc13ea17f4d6c9a70c06d7677ba
hadiRazal <hadhirasal22@gmail.com>
2024-02-03 12:28:57 +0530

    added update in delete category

--- app/api/category/[categoryId]/route.ts
+++ app/api/category/[categoryId]/route.ts
@@ -6,37 +6,69 @@ import { NextParsedUrlQuery } from 'next/dist/server/request-meta';
 
 import { NextRequest, NextResponse } from 'next/server';
 
+
+
+// getting all products based on its category
+
 export async function GET(req: NextRequest, { params }: any) {
     try {
         connectMongoDB()
 
         const categoryId = params.categoryId
 
+
+        const category = await CategoryModel.find({ _id: categoryId })
+
         const products = await ProductModel.find({
             category: categoryId
         })
-        return NextResponse.json({ products, message: "fetched all products", success: true })
+        return NextResponse.json({ category, products, message: "fetched all products by category", success: true })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ error, success: false })
 
     }
 }
+
+
+
+// deleting a category will also delete all its products
+
 
 export async function DELETE(req: NextRequest, { params }: any) {
     try {
-        connectMongoDB()
+        connectMongoDB();
 
-        const categoryId = params.categoryId
+        const categoryId = params.categoryId;
 
-        const category = await CategoryModel.findByIdAndDelete({ _id: categoryId })
+        
+        // set null calue to category ids without deleting products
+
+        // const products = await ProductModel.updateMany(
+        //     { category: categoryId },
+        //     { $set: { category: null } }
+        // );
 
 
+        // delete all the products in the category
 
-        return NextResponse.json({ message: "Category deleted successfully", success: true })
+        await ProductModel.deleteMany({
+            category: categoryId
+        })
+
+        // Delete the category
+        const category = await CategoryModel.findByIdAndDelete(categoryId);
+
+        if (!category) {
+            return NextResponse.json({ message: 'Category not found', success: false });
+        }
+
+        return NextResponse.json({ message: 'Category deleted successfully', success: true });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error, success: false })
-
+        console.error(error);
+        return NextResponse.json({ error: 'Internal Server Error', success: false });
     }
 }
+
+
+
