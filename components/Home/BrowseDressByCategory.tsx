27b354dd8a3642a9813ef21a11aa3d8954805a6f
hadiRazal <hadhirasal22@gmail.com>
2024-02-04 23:57:27 +0530

    added changes to product card and browse category

--- components/Home/BrowseDressByCategory.tsx
+++ components/Home/BrowseDressByCategory.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const BrowseDressByCategory = () => {
     return (
-        <div className=' lg:px-10 px-5 mb-10'>
-            <div className='h-full rounded-2xl bg-td-secondary gap-5 flex flex-col pb-16 lg:px-20 px-5'>
+        <div className=' lg:px-10 px-5'>
+            <div className='h-full rounded-2xl bg-td-secondary gap-5 flex flex-col pb-5 lg:px-20 px-5'>
                 <h1 className='text-center font-extrabold text-white lg:text-[50px] md:text-[30px] text-[20px] mt-5  '>Browse Dress By Category</h1>
                 <div className='flex flex-col items-center justify-center w-full gap-2 font-medium'>
                     <div className='bg-emerald-50 w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
