b5366eb263927d450bb0c5f157fe1bdee551122a
hadiRazal <hadhirasal22@gmail.com>
2024-02-06 00:51:27 +0530

    category list

--- app/admin-panel/view-categories/page.tsx
+++ app/admin-panel/view-categories/page.tsx
@@ -4,18 +4,22 @@
 import { Category } from '@/types'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { PulseLoader } from 'react-spinners'
 
 const ViewAllCategories = () => {
 
 
-    const [categories, setCategories] = useState<Category>()
+    const [categories, setCategories] = useState<Category[]>([])
 
 
     const fetchCategory = async () => {
         try {
-            const response = await axios.get('/api/categories')
+            const response = await axios.get('/api/category')
 
-            console.log(response)
+            console.log(response.data.tdCategory)
+
+            setCategories(response.data.tdCategory)
+
         } catch (error) {
             console.log(error)
         }
@@ -25,9 +29,42 @@ const ViewAllCategories = () => {
         fetchCategory()
     })
 
-    return (
-        <div className='md:px-10 px-5 py-10 flex flex-wrap items-center justify-center gap-3'>
 
+    // on loading of categories
+    if (categories.length === 0) {
+        return (
+            <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
+                <div>
+                    <p className='font-medium'>No Categoies Available</p>
+                </div>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
+            <div>
+                <h1 className='text-[20px] font-semibold'>All Categories</h1>
+            </div>
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
+            {categories?.map((cat: Category, i) => (
+                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            ))}
         </div>
     )
 }
