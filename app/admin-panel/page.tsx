954003af66fe796af746dbc8434dcdf36fd3a5d6
hadiRazal <hadhirasal22@gmail.com>
2024-02-06 00:29:30 +0530

    added product and category view page in admin panel

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -3,27 +3,16 @@
 // import { useAdmin } from '@/context/admin';
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const AdminPanel = () => {
-
-    // const { admin: adminDetails } = useAdmin();
-    // const router = useRouter()
-
-    // useEffect(() => {
-    //     if (adminDetails.token) {
-    //         try {
-    //             router.push('/admin-panel');
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    // }, [adminDetails.token, router]);
 
     return (
         <div className='h-[50vh] flex flex-col items-center justify-center gap-4 '>
             <Link href="/admin-panel/create-product" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Add Product</Link>
             <Link href="/admin-panel/create-category" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Add Category</Link>
+            <Link href="/admin-panel/view-products" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Product</Link>
+            <Link href="/admin-panel/view-categories" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Category</Link>
         </div>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,35 @@ import Image from 'next/image';
 import { CiSearch, CiUser, CiShoppingCart } from "react-icons/ci";
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useUser } from '@/context/useUser';
 
 
 const Header = () => {
 
+    const { LogOut, currentUser } = useUser()
+
     const router = useRouter()
+
+    const onAccountClick = () => {
+        try {
+
+            if (!currentUser) {
+                return router.push('/account')
+            }
+
+            if (currentUser?.token && currentUser.isAdmin === true) {
+                return router.push('/admin-panel')
+            }
+
+            if (currentUser?.token && currentUser.isAdmin === false) {
+                return router.push(`/account/${currentUser.userId}`)
+            }
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className='bg-td-secondary flex items-center justify-between px-3 lg:px-10'>
@@ -19,7 +43,7 @@ const Header = () => {
             <div className='flex items-center justify-center gap-2'>
                 <CiSearch onClick={() => router.push('/search')} className='text-white cursor-pointer' size={24} />
                 <CiShoppingCart onClick={() => router.push('/cart')} className='text-white cursor-pointer' size={24} />
-                <CiUser onClick={() => { router.push("/account") }} className='text-white cursor-pointer' size={24} />
+                <CiUser onClick={onAccountClick} className='text-white cursor-pointer' size={24} />
             </div>
         </div>
     )
