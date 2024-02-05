dd30f3054239d7d9f60952bc28edcf5f067fb1bd
hadiRazal <hadhirasal22@gmail.com>
2024-02-06 01:15:26 +0530

    made updates in admin  panel

--- app/admin-panel/view-categories/page.tsx
+++ app/admin-panel/view-categories/page.tsx
@@ -3,6 +3,7 @@
 
 import { Category } from '@/types'
 import axios from 'axios'
+import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { PulseLoader } from 'react-spinners'
 
@@ -27,7 +28,7 @@ const ViewAllCategories = () => {
 
     useEffect(() => {
         fetchCategory()
-    })
+    }, [])
 
 
     // on loading of categories
@@ -35,7 +36,7 @@ const ViewAllCategories = () => {
         return (
             <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
                 <div>
-                    <p className='font-medium'>No Categoies Available</p>
+                    <p className='font-medium'>No Categories Available</p>
                 </div>
             </div>
         )
@@ -45,26 +46,20 @@ const ViewAllCategories = () => {
     return (
         <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
             <div>
-                <h1 className='text-[20px] font-semibold'>All Categories</h1>
+                <h1 className='text-[30px] font-bold text-td-secondary'>All Categories</h1>
             </div>
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
+            <div className='flex flex-wrap items-center justify-center gap-5'>                {categories?.map((cat: Category, i) => (
+                <div key={i} className='w-[300px] pb-10 flex flex-col bg-td-secondary rounded-2xl'>
+                    <Image src={cat.imageURL} style={{ objectFit: "cover" }} alt='Image' className='rounded-2xl min-h-[300px]' height={400} width={400} />
+                    <span className='w-full  text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat.categoryName}</span>
+                    <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
+                        <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>Edit</button>
+                        <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>View Products</button>
+                        <button className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium'>Delete</button>
+                    </div>
+                </div>
             ))}
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
-            ))}
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
-            ))}
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
-            ))}
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
-            ))}
-            {categories?.map((cat: Category, i) => (
-                <span key={i} className='bg-td-secondary w-full text-white px-5 py-3 rounded-2xl'>{cat.categoryName}</span>
-            ))}
+            </div>
         </div>
     )
 }
