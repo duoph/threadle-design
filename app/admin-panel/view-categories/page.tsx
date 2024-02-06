e1c2221f5eae359a60af90002e55b2924fc90aa2
hadiRazal <hadhirasal22@gmail.com>
2024-02-06 10:36:55 +0530

    added default image

--- app/admin-panel/view-categories/page.tsx
+++ app/admin-panel/view-categories/page.tsx
@@ -5,7 +5,6 @@ import { Category } from '@/types'
 import axios from 'axios'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
-import { PulseLoader } from 'react-spinners'
 
 const ViewAllCategories = () => {
 
@@ -51,9 +50,9 @@ const ViewAllCategories = () => {
             <div className='flex flex-wrap items-center justify-center gap-5'>                {categories?.map((cat: Category, i) => (
                 <div key={i} className='relative w-[300px] pb-10 flex flex-col bg-td-secondary rounded-2xl'>
                     {!cat?.imageURL && (
-                        <Image src={"/noImage.jpg"} alt='Image' className='absolute rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
+                        <Image src={"/noImage.jpg"} style={{ objectFit: "cover" }} alt='Image' className='absolute rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
                     )}
-                    <Image src={cat.imageURL} style={{ objectFit: "cover", background: "/noImage.jpg" }} alt='Image' className='rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
+                    <Image src={cat.imageURL} style={{ objectFit: "cover" }} alt='Image' className='rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
                     <span className='w-full  text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat.categoryName}</span>
                     <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
                         <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>Edit</button>
