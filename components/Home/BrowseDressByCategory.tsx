fd43a9d942270ee96c5587be27fbac60a8ed07a1
hadiRazal <hadhirasal22@gmail.com>
2024-02-06 13:57:58 +0530

    made changes in shop page

--- components/Home/BrowseDressByCategory.tsx
+++ components/Home/BrowseDressByCategory.tsx
@@ -1,30 +1,50 @@
+"use client"
+
+import { Category } from '@/types'
+import axios from 'axios'
 import Image from 'next/image'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 const BrowseDressByCategory = () => {
+
+
+
+
+    const [categories, setCategories] = useState<Category[]>([])
+
+
+    const router = useRouter()
+
+    const fetchCategory = async () => {
+        try {
+            const response = await axios.get('/api/category')
+
+            console.log(response.data.tdCategory)
+
+            setCategories(response.data.tdCategory)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchCategory()
+    }, [])
+
+
+
     return (
         <div className=' lg:px-10 px-5'>
-            <div className='h-full rounded-2xl bg-td-secondary gap-5 flex flex-col pb-5 lg:px-20 px-5'>
+            <div className='h-full rounded-2xl bg-td-secondary gap-5 flex flex-col pb-10 lg:px-20 px-5'>
                 <h1 className='text-center font-extrabold text-white lg:text-[50px] md:text-[30px] text-[20px] mt-5  '>Browse Dress By Category</h1>
-                <div className='flex flex-col items-center justify-center w-full gap-2 font-medium'>
-                    <div className='bg-emerald-50 w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px] text-center'>Party Wear</h1>
-                    </div>
-                    <div className='bg-emerald-50   w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px] text-center'>Semi Party Wear</h1>
-                    </div>
-                    <div className='bg-emerald-50   w-full rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px]  text-center'>Wedding</h1>
-                    </div>
-                    <div className='bg-emerald-50   w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px] text-center'>Saree</h1>
-                    </div>
-                    <div className='bg-emerald-50   w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px] text-center'>Kurta</h1>
-                    </div>
-                    <div className='bg-emerald-50   w-full  rounded-2xl flex items-center justify-center overflow-hidden cursor-pointer'>
-                        <h1 className='lg:text-[30px] text-[20px] text-center'>Churidar</h1>
-                    </div>
+                <div className='flex flex-wrap items-center justify-center w-full gap-2 font-medium text-sm'>
+                    {categories.map((category: Category) => (
+                        <div className='bg-emerald-50 md:w-1/3 w-full  rounded-2xl py-2 flex items-center justify-center overflow-hidden cursor-pointer'>
+                            <h1 className='lg:text-[30px] text-[20px] text-center'>{category.categoryName}</h1>
+                        </div>
+                    ))}
                 </div>
             </div >
         </div >
