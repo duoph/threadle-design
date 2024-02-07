f0b7ea102e318bf72bd3d56dc52a03e7c8e726ef
hadiRazal <hadhirasal22@gmail.com>
2024-02-07 16:39:05 +0530

    added category product view in admin panel

--- app/admin-panel/view-categories/page.tsx
+++ app/admin-panel/view-categories/page.tsx
@@ -15,6 +15,7 @@ const ViewAllCategories = () => {
 
     const router = useRouter()
 
+
     const fetchCategory = async () => {
         try {
             const response = await axios.get('/api/category')
@@ -44,26 +45,31 @@ const ViewAllCategories = () => {
         )
     }
 
-
     return (
         <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
             <div>
                 <h1 className='text-[30px] font-bold text-td-secondary'>All Categories</h1>
             </div>
-            <div className='flex flex-wrap items-center justify-center gap-5'>                {categories?.map((cat: Category, i) => (
-                <div key={i} className='relative w-[300px] pb-10 flex flex-col bg-td-secondary rounded-2xl'>
-                    {!cat?.imageURL && (
-                        <Image src={"/noImage.jpg"} style={{ objectFit: "cover" }} alt='Image' className='absolute rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
-                    )}
-                    <Image src={cat?.imageURL} style={{ objectFit: "cover" }} alt='Image' className='rounded-2xl min-h-[300px]  max-h-[300px]' height={400} width={400} />
-                    <span className='w-full  text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat?.categoryName}</span>
-                    <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
-                        <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium' onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}>Edit</button>
-                        <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>View Products</button>
-                        <button className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium' >Delete</button>
+            <div className='flex flex-wrap items-center justify-center gap-5'>
+                {categories.map((cat: Category, i) => (
+                    <div key={i} className='relative w-[300px] pb-10 flex flex-col bg-td-secondary rounded-2xl'>
+                        <Image
+                            src={cat.imageURL || "/noImage.jpg"}
+                            alt='Image'
+                            className='rounded-2xl min-h-[300px] max-h-[300px]'
+                            style={{ objectFit: "cover" }}
+                            height={400}
+                            width={400}
+                            onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}
+                        />
+                        <span className='w-full text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat.categoryName}</span>
+                        <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
+                            <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium' onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}>Edit</button>
+                            <button onClick={() => router.push(`/admin-panel/view-products/${cat._id}`)} className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>View Products</button>
+                            <button className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium'>Delete</button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
             </div>
         </div>
     )
