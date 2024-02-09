61d3cf96f42d85aff6b0944a57d51eecfde0e59a
hadiRazal <hadhirasal22@gmail.com>
2024-02-09 12:19:06 +0530

    added delete confirmation modal

--- app/admin-panel/view-categories/page.tsx
+++ app/admin-panel/view-categories/page.tsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 const ViewAllCategories = () => {
     const [categories, setCategories] = useState<Category[]>([])
@@ -31,10 +32,10 @@ const ViewAllCategories = () => {
         try {
             if (idToDelete) {
                 // Perform deletion
-                await axios.delete(`/api/category/${idToDelete}`)
+                const res = await axios.delete(`/api/category/${idToDelete}`)
                 // Update categories after deletion
                 fetchCategory()
-                // Reset state
+                toast.success(res.data.message)
                 setDeleteConfirm(false)
                 setIdToDelete(null)
             }
@@ -55,46 +56,49 @@ const ViewAllCategories = () => {
     }
 
     return (
-        <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3'>
-            <div>
-                <h1 className='text-[30px] font-bold text-td-secondary'>All Categories</h1>
-            </div>
-
+        <div className='min-h-screen flex items-center justify-center md:px-10 px-5'>
             {deleteConfirm && (
-                <div className='flex gap-3 flex-col sticky bg-td-primary py-5 p-5 shadow-2xl z-10 rounded-2xl'>
-                    <h1>Are you sure?</h1>
-                    <p>
-                        Lorem, ipsum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, vero.
-                    </p>
-                    <button className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium' onClick={handleDelete}>Delete</button>
-                    <button className='bg-white w-full px-4 py-3 rounded-xl font-medium' onClick={() => setDeleteConfirm(false)}>Cancel</button>
+                <div className=' fixed  top-0 left-0 w-full h-full flex items-center justify-center z-10 md:px-10 px-5'>
+                    <div className='bg-td-primary p-6 rounded-md shadow-2xl'>
+                        <h1 className='text-xl font-bold'>Are you sure?</h1>
+                        <p className='text-gray-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, vero.</p>
+                        <div className='mt-4 flex justify-center'>
+                            <button className='bg-red-600 text-white px-4 py-2 rounded-md mr-2' onClick={handleDelete}>Delete</button>
+                            <button className='bg-gray-300 text-gray-800 px-4 py-2 rounded-md' onClick={() => setDeleteConfirm(false)}>Cancel</button>
+                        </div>
+                    </div>
                 </div>
             )}
 
-            <div className='flex flex-wrap items-center justify-center gap-5'>
-                {categories.map((cat: Category, i) => (
-                    <div key={i} className='relative w-[300px] pb-10 flex flex-col bg-td-secondary rounded-2xl'>
-                        <Image
-                            quality={50}
-                            src={cat.imageURL || "/noImage.jpg"}
-                            alt='Image'
-                            className='rounded-2xl min-h-[300px] max-h-[300px]'
-                            style={{ objectFit: "cover" }}
-                            height={400}
-                            width={400}
-                            onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}
-                        />
-                        <span className='w-full text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat.categoryName}</span>
-                        <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
-                            <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium' onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}>Edit</button>
-                            <button onClick={() => router.push(`/admin-panel/view-products/${cat._id}`)} className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>View Products</button>
-                            <button onClick={() => { setDeleteConfirm(true); setIdToDelete(cat._id) }} className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium'>Delete</button>
+            <div className='min-h-[80vh] md:px-10 px-5 py-10 flex flex-col items-center justify-center gap-3 '>
+                <div>
+                    <h1 className='text-[30px] font-bold text-td-secondary'>All Categories</h1>
+                </div>
+
+                <div className='flex flex-wrap items-center justify-center gap-5'>
+                    {categories.map((cat: Category, i) => (
+                        <div key={i} className='relative w-[300px] shadow-2xl pb-10 flex flex-col bg-td-secondary rounded-2xl'>
+                            <Image
+                                quality={50}
+                                src={cat.imageURL || "/noImage.jpg"}
+                                alt='Image'
+                                className='rounded-2xl min-h-[300px] max-h-[300px]'
+                                style={{ objectFit: "cover" }}
+                                height={400}
+                                width={400}
+                                onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}
+                            />
+                            <span className='w-full text-white px-5 py-5 text-center rounded-2xl cursor-pointer'>{cat.categoryName}</span>
+                            <div className='flex flex-col w-full gap-4 items-center justify-evenly text-white px-5'>
+                                <button className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium' onClick={() => router.push(`/admin-panel/edit-category/${cat._id}`)}>Edit</button>
+                                <button onClick={() => router.push(`/admin-panel/view-products/${cat._id}`)} className='bg-td-primary w-full px-4 py-3 rounded-xl font-medium'>View Products</button>
+                                <button onClick={() => { setDeleteConfirm(true); setIdToDelete(cat._id) }} className='bg-red-600 w-full px-4 py-3 rounded-xl font-medium'>Delete</button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     )
 }
-
 export default ViewAllCategories
