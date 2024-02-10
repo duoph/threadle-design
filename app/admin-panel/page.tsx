161d7c7138e8c60ef18e3e69fa348e1259e43ce3
hadiRazal <hadhirasal22@gmail.com>
2024-02-10 14:03:59 +0530

    product fetch sorted

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -8,11 +8,12 @@ import React from 'react'
 const AdminPanel = () => {
 
     return (
-        <div className='h-[50vh] flex flex-col items-center justify-center gap-4 '>
+        <div className='py-10 flex flex-col items-center justify-center gap-4 '>
             <Link href="/admin-panel/create-product" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Add Product</Link>
             <Link href="/admin-panel/create-category" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Add Category</Link>
             <Link href="/admin-panel/view-products" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Product</Link>
             <Link href="/admin-panel/view-categories" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Category</Link>
+            <Link href="/" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Orders</Link>
         </div>
     )
 }
