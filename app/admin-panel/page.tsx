f0a83ac2db578a182a4c55660d2ee8ce37c13f0a
hadiRazal <hadhirasal22@gmail.com>
2024-02-12 00:22:32 +0530

    made changes in admin panel

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -1,11 +1,22 @@
 "use client"
 
+import { useUser } from '@/context/useUser';
 // import { useAdmin } from '@/context/admin';
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const AdminPanel = () => {
+
+
+    const { LogOut, currentUser } = useUser()
+    const router = useRouter()
+
+
+    if (!currentUser?.token || currentUser?.isAdmin === false) {
+        return router.push('/account/login')
+    }
+
 
     return (
         <div className='py-10 flex flex-col items-center justify-center gap-4 '>
@@ -14,6 +25,7 @@ const AdminPanel = () => {
             <Link href="/admin-panel/view-products" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Product</Link>
             <Link href="/admin-panel/view-categories" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>View All Category</Link>
             <Link href="/" className='bg-red-600 px-10 py-4 rounded-2xl text-white'>Orders</Link>
+            <button className='bg-red-600 px-10 py-4 rounded-2xl text-white' onClick={LogOut}>Logout</button>
         </div>
     )
 }
