6a4ea6cc6143779839f1c8f7a1313f245c02de24
hadiRazal <hadhirasal22@gmail.com>
2024-03-01 15:54:24 +0530

    wishlist api

--- app/api/wishlist/[productId]/route.ts
+++ app/api/wishlist/[productId]/route.ts
@@ -1,17 +1,69 @@
-import userModel from "@/models/userModel"
-import { NextRequest, NextResponse } from "next/server"
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import userModel from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+
+// adding product to the wishlist
 
 export async function GET(req: NextRequest, { params }: any) {
-
     try {
-        const productId = params.productId
+        const userId = getDataFromToken(req);
+        const productId = params.productId;
 
-        const addToWishList = await userModel.find
+        console.log(userId)
 
-        return NextResponse.json(productId)
+        if (!userId) {
+            // Token is missing or invalid
+            return NextResponse.json({ message: "Token is missing or invalid", success: false });
+        }
+
+        // Find the user based on userId from the token
+        const user = await userModel.findById({ _id: userId });
+
+        if (!user) {
+            // User not found
+            return NextResponse.json({ message: "User not found", success: false });
+        }
+
+        // Update user's wishlist by adding productId to the array
+        await userModel.findByIdAndUpdate(user._id, { $addToSet: { wishList: productId } });
+
+        return NextResponse.json({ message: "Product added to wishlist", success: true });
     } catch (error) {
-        console.log(error)
+        console.error("Error:", error);
+        return NextResponse.json({ message: "Internal Server Error", success: false });
     }
+}
 
 
-} 
+// Remove product from the wishlist
+
+export async function PUT(req: NextRequest, { params }: any) {
+    try {
+        const userId = getDataFromToken(req);
+        const productId = params.productId;
+
+        console.log(userId)
+
+        if (!userId) {
+            // Token is missing or invalid
+            return NextResponse.json({ message: "Token is missing or invalid", success: false });
+        }
+
+        // Find the user based on userId from the token
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            // User not found
+            return NextResponse.json({ message: "User not found", success: false });
+        }
+
+        // Update user's wishlist by removing productId from the array
+        await userModel.findByIdAndUpdate(userId, { $pull: { wishList: productId } });
+
+        return NextResponse.json({ message: "Product removed from wishlist", success: true });
+    } catch (error) {
+        console.error("Error:", error);
+        return NextResponse.json({ message: "Internal Server Error", success: false });
+    }
+}
---
+++ helpers/getDataFromToken.ts
@@ -0,0 +1,21 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { NextRequest, NextResponse } from "next/server";
+
+interface MyToken extends JwtPayload {
+    _id: string; // Assuming _id is of type string
+    // Add other properties if they exist in your token
+}
+
+export const getDataFromToken = (req: NextRequest) => {
+    try {
+        const token = req.headers.get("Authorization");
+        console.log(token)
+        const decodedToken = jwt.verify(token as string, process.env.NEXT_PUBLIC_JWT_SECRET as string) as MyToken;
+
+        return decodedToken._id
+
+    } catch (error) {
+        console.error("Error decoding JWT token:", error);
+    }
+};
+
