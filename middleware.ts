36b4e377aa5f6d8ed3f98554ffd606f53042eacf
hadiRazal <hadhirasal22@gmail.com>
2024-03-03 14:01:18 +0530

    cookies

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,31 @@
+import jwt from 'jsonwebtoken';
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getDataFromToken } from './helpers/getDataFromToken';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL('/home', request.url))
+
+    const path = request.nextUrl.pathname
+
+    const token = request.cookies.get("token")?.value || ""
+
+    const decodedToken: any = getDataFromToken(request)
+
+    console.log("This is the token data", decodedToken.isAdmin)
+
+    const isPublicPath = path === "/account/login" || "/account/create-account"
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/account', request.url))
+    }
+
+    if (path.startsWith("/admin-panel/") && decodedToken?.isAdmin === false) {
+        return NextResponse.redirect(new URL('/account', request.url))
+    }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: '/about/:path*',
+    matcher: ['/admin-panel/:path*', '/account/[userId]', "/account/login", "/account/create-account"]
 }
