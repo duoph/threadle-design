c90c17b86b1f5937b1829393eab83abdff1d398d
hadiRazal <hadhirasal22@gmail.com>
2024-03-05 01:17:30 +0530

    updated product page

--- app/shop/[productId]/page.tsx
+++ app/shop/[productId]/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import ProductContainerWithCategory from '@/components/ProductContainerWithCategory'
-import { Product } from '@/types'
-import axios from 'axios'
+import { Category, Product } from '@/types'
+import axios, { AxiosResponse } from 'axios'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
@@ -16,15 +16,18 @@ const ProductPage = () => {
 
   const { productId } = useParams()
   const [product, setProduct] = useState<Product>()
+  const [selectedSize, setSelectedSize] = useState<string>()
+  const [quantity, setQuantity] = useState<number>(1)
+  const [categories, setCategories] = useState<Category[] | []>([]);
 
 
-  const router = useRouter()
+
+  const size = ["S", "M", "L", "XL", "2XL", "3XL"]
+
 
   const fetchProduct = async () => {
     try {
       const response = await axios.get(`/api/product/${productId}`)
-
-      console.log(response.data.product)
 
       setProduct(response.data.product[0])
 
@@ -34,18 +37,36 @@ const ProductPage = () => {
   }
 
 
+  const fetchCategory = async () => {
+    try {
+      const response = await axios.get("/api/category/" + product?.categoryId);
+      console.log(response.data);
+      setCategories(response.data.tdCategory);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
   useEffect(() => {
+    fetchCategory()
     fetchProduct()
   }, [])
 
-  console.log(product)
+
+  const handleQuantity = (action: string) => {
+    if (action === "increment") {
+      setQuantity(quantity + 1);
+    } else if (action === "decrement" && quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
 
 
   return (
     <div className='w-full px-5 py-3 md:px-10 flex flex-col gap-3 mb-5'>
-      <div className=''>
-        <h1 >Shop -  Categoy  - 123</h1>
-      </div>
       <div className='flex flex-col w-full gap-3'>
         <div className='flex flex-row w-full gap-2 items-center justify-center bg-slate-200 '>
           <div>
@@ -58,7 +79,7 @@ const ProductPage = () => {
             <Image src={product?.moreImagesURLs[3] || "/greendress.png"} alt='greenDress' width={100} height={100} />         </div>
         </div>
         <div className='flex flex-col gap-5 items-start justify-start md:w-1/2'>
-          <h1 className='text-lg font-bold'>Green Dress</h1>
+          <h1 className='text-lg font-medium'>{product?.title}</h1>
           <span className='text-start break-all flex items-center justify-start'>
             <FaStar size={24} className='text-yellow-300' />
             <FaStar size={24} className='text-yellow-300' />
@@ -67,27 +88,24 @@ const ProductPage = () => {
             <CiStar size={24} className='text-yellow-300' />
             <span className='font-thin text-sm px-1'>4/5</span>
           </span>
-          <p className='text-lg font-semibold'>&#8377;{product?.regularPrice}</p>
+          <p className='text-lg font-medium'>&#8377;{product?.regularPrice}</p>
           <div className='flex flex-col gap-5 w-full'>
             <p>Select Colors</p>
             <div className='flex gap-3'>
-              <span className='h-[25px] bg-red-700 w-[25px] rounded-[50%] inline-block'></span>
-              <span className='h-[25px] bg-green-700 w-[25px] rounded-[50%] inline-block'></span>
-              <span className='h-[25px] bg-blue-700 w-[25px] rounded-[50%] inline-block'></span>
+              <span className='h-[35px] bg-red-700 w-[35px] rounded-[50%] inline-block'></span>
+              <span className='h-[35px] bg-green-700 w-[35px] rounded-[50%] inline-block'></span>
+              <span className='h-[35px] bg-blue-700 w-[35px] rounded-[50%] inline-block'></span>
             </div>
             <div className='flex gap-3 font-light'>
-              <button className=' px-4 py-2 bg-gray-200 rounded-2xl '>S</button>
-              <button className='bg-td-primary text-white px-4 py-2  rounded-2xl '>M</button>
-              <button className=' px-4 py-2 bg-gray-200 rounded-2xl '>L</button>
-              <button className=' px-4 py-2 bg-gray-200 rounded-2xl '>XL</button>
-              <button className=' px-4 py-2 bg-gray-200 rounded-2xl '>2XL</button>
-              <button className=' px-4 py-2 bg-gray-200 rounded-2xl '>3XL</button>
+              {size?.map((size) => (
+                <button onClick={() => setSelectedSize(size)} className={`px-4 py-2  rounded-2xl ${selectedSize === size ? "bg-td-primary text-white" : "bg-gray-200"}`}>{size}</button>
+              ))}
             </div>
             <div className='flex gap-3 font-semibold w-full'>
               <span className='bg-gray-200 flex items-center justify-between gap-4 px-8 py-2 rounded-2xl w-1/2'>
-                <span>-</span>
-                <span>2</span>
-                <span>+</span>
+                <span className='cursor-pointer' onClick={() => handleQuantity("decrement")}>-</span>
+                <span>{quantity}</span>
+                <span className='cursor-pointer' onClick={() => handleQuantity("increment")}>+</span>
               </span>
               <button className='w-1/2 py-2 bg-td-primary rounded-2xl text-white'> Add to Cart </button>
             </div>
@@ -98,16 +116,17 @@ const ProductPage = () => {
 
       <div className='mt-5'>
 
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore voluptatibus saepe voluptas quae sit harum cumque. Amet veritatis accusamus asperiores enim voluptas et sint, ad aliquam iure, autem fuga itaque error voluptatibus, hic laboriosam corrupti! Laborum minima debitis placeat, eius animi asperiores ad earum repudiandae. Quaerat ea natus doloribus suscipit?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum et culpa esse magnam nam eaque tenetur repellat numquam libero hic, repudiandae nihil est quos recusandae doloremque adipisci maxime, non in labore eius? Obcaecati, in ducimus ea id aliquam possimus optio at iste illum quisquam porro a nobis cupiditate laborum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, eos doloremque recusandae, sit voluptas error facere quaerat aliquam provident, voluptate accusantium suscipit fugit minus aut enim velit molestiae quis qui maiores optio soluta rem!</p>
+        <p>{product?.desc}</p>
 
       </div>
 
-      {/* <div>
-        <ProductContainerWithCategory />
-      </div> */}
+      <div>
+        {/* <ProductContainerWithCategory category={catego} /> */}
+      </div>
 
     </div >
   )
+
 }
 
 export default ProductPage
