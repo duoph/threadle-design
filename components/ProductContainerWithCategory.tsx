d0b5bef6f1008bff55478ad8bcd00770c8bd697c
hadiRazal <hadhirasal22@gmail.com>
2024-03-08 23:29:48 +0530

    error fix api

--- components/ProductContainerWithCategory.tsx
+++ components/ProductContainerWithCategory.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useEffect, useRef, useState } from 'react';
 import ProductCard from './ProductCard';
 import { GoChevronRight, GoChevronLeft } from "react-icons/go";
@@ -6,38 +5,35 @@ import { useRouter } from 'next/navigation';
 import { Category, Product } from '@/types';
 import axios from 'axios';
 
-
 interface ProductContainerProps {
     category?: any
     categoryId?: string
     title?: string
     productNotToshow?: string
-
 }
-
 
 const ProductContainerWithCategory = ({ category, categoryId, title, productNotToshow }: ProductContainerProps) => {
     const scrollContainerRef = useRef<HTMLDivElement>(null);
     const [products, setProducts] = useState<Product[]>([]);
-
-
     const router = useRouter();
 
     const fetchCategoryProducts = async () => {
         try {
             const response = await axios.get(`/api/category/${categoryId || category._id}`);
-            console.log(response)
-            setProducts(response?.data?.products);
+            console.log(response);
+            if (response.data.success === true) {
+                setProducts(response?.data?.products);
+            } else {
+                setProducts([])
+            }
         } catch (error) {
             console.log(error);
         }
     };
 
-
     useEffect(() => {
         fetchCategoryProducts();
     }, [category, categoryId]);
-
 
     const handleScrollRight = () => {
         try {
@@ -68,23 +64,29 @@ const ProductContainerWithCategory = ({ category, categoryId, title, productNotT
     };
 
     return (
-        <div className='flex flex-col items-center justify-center mt-10 w-full mb-5'>
-            <h1 className='text-[35px] font-extrabold'>{title || category?.categoryName}</h1>
-            <div className='relative w-full px-2'>
-                <GoChevronLeft onClick={handleScrollLeft} className='md:hover:scale-110 z-10 absolute top-[200px] bg-black text-white  left-8 rounded-full cursor-pointer' size={30} />
-                <GoChevronRight onClick={handleScrollRight} className='md:hover:scale-110 z-10 absolute top-[200px] bg-black text-white right-8 rounded-full cursor-pointer' size={30} />
-                <div ref={scrollContainerRef} className='flex hideScrollBar hideScrollBar overflow-x-scroll justify-start items-center gap-5 h-[450px]'>
-                    {products?.map((product) => (<>
-                        {productNotToshow === product._id ? null : (<ProductCard key={product._id} product={product} getProducts={fetchCategoryProducts} />)}
-                    </>))}
-                </div>
-            </div>
-            <div className='px-5 w-full flex items-center justify-center'>
-                <button onClick={() => router.push(`/category/${category._id}`)} className='text-lg border md:w-auto w-full rounded-2xl px-5 py-2  relative z-10'>
-                    View All
-                </button>
-            </div>
-        </div>
+        <>
+            {!products ? null : (
+                <div className='flex flex-col items-center justify-center mt-10 w-full mb-5'>
+                    <h1 className='text-[35px] font-extrabold'>{title || category?.categoryName}</h1>
+                    <div className='relative w-full px-2'>
+                        <GoChevronLeft onClick={handleScrollLeft} className='md:hover:scale-110 z-10 absolute top-[200px] bg-black text-white  left-8 rounded-full cursor-pointer' size={30} />
+                        <GoChevronRight onClick={handleScrollRight} className='md:hover:scale-110 z-10 absolute top-[200px] bg-black text-white right-8 rounded-full cursor-pointer' size={30} />
+                        <div ref={scrollContainerRef} className='flex hideScrollBar hideScrollBar overflow-x-scroll justify-start items-center gap-5 h-[450px]'>
+                            {products?.map((product) => (
+                                <div key={product._id}>
+                                    {productNotToshow === product._id ? null : <ProductCard product={product} getProducts={fetchCategoryProducts} />}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='px-5 w-full flex items-center justify-center'>
+                        <button onClick={() => router.push(`/category/${category._id}`)} className='text-lg border md:w-auto w-full rounded-2xl px-5 py-2  relative z-10'>
+                            View All
+                        </button>
+                    </div>
+                </div >
+            )}
+        </>
     );
 };
 
