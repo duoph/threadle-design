9b42643d6547950d33b9d675b267fa40f954096c
hadiRazal <hadhirasal22@gmail.com>
2024-03-10 22:12:22 +0530

    like and dislike error fixed

--- models/cartItemModel.ts
+++ models/cartItemModel.ts
@@ -1,13 +1,21 @@
-import mongoose, { Mongoose, Schema } from "mongoose";
+import mongoose, { Schema, Model, Document } from "mongoose";
 
-interface CartItemProps {
-    productId: mongoose.Types.ObjectId;
+interface CartItemProps extends Document {
+    productId: Schema.Types.ObjectId;
     quantity: number;
-    userId: mongoose.Types.ObjectId;
-    color?: string
-    price: number
+    userId: Schema.Types.ObjectId;
+    color?: string;
+    price: number;
 }
 
 const cartItemSchema = new Schema<CartItemProps>({
+    productId: { type: Schema.Types.ObjectId, required: true },
+    quantity: { type: Number, required: true },
+    userId: { type: Schema.Types.ObjectId, required: true },
+    color: { type: String },
+    price: { type: Number, required: true }
+}, { timestamps: true });
 
-})
+const CartModel = mongoose.models.cart || mongoose.model<CartItemProps>('cart', cartItemSchema);
+
+export default CartModel;
