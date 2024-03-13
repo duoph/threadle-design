6ee1a8ad5d32f17e6be32ef040d796482e6b8b08
hadiRazal <hadhirasal22@gmail.com>
2024-03-14 02:17:38 +0530

    updates

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -18,11 +18,13 @@ export async function GET(req: NextRequest) {
 
 
         const wishList = await user?.wishList || [];
+        console.log(wishList)
         const wishListIds = await user?.wishList?.map((item: any) => item._id);
+        console.log(wishListIds)
 
         return NextResponse.json({ message: "Fetched user wishlist", success: true, wishList, wishListIds });
     } catch (error: any) {
         console.error('Error while fetching wishlist:', error);
-        return NextResponse.json({ message: "Error while fetching wishlist", success: false, error: error.message });
+        return NextResponse.json({ message: "Error while fetching wishlist", success: false, error: error.json });
     }
 }
