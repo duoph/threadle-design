3cee188d9bffcb8b8f2c16e52fae4fc4c42fd77d
hadiRazal <hadhirasal22@gmail.com>
2024-03-16 19:45:06 +0530

    uodate

--- app/account/[userId]/page.tsx
+++ app/account/[userId]/page.tsx
@@ -3,7 +3,7 @@
 import { useUser } from '@/context/useUser'
 import axios from 'axios'
 import { useParams, useRouter } from 'next/navigation'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 import { FaPhoneAlt } from 'react-icons/fa'
 import { FaAddressCard } from 'react-icons/fa6'
@@ -15,45 +15,53 @@ const UserProfile = () => {
     const { LogOut, currentUser } = useUser()
     const { userId } = useParams()
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        address: ''
+        name: currentUser?.name,
+        email: currentUser?.email,
+        phone: currentUser?.phone,
+        address: currentUser?.address,
     })
 
-    useEffect(() => {
-        getUser()
-    }, [])
 
-    const getUser = async () => {
-        try {
-            const res = await axios.get(`/api/user/${userId}`)
-            const userData = res.data
-            setFormData({
-                name: userData.name,
-                email: userData.email,
-                phone: userData.phone,
-                address: userData.address
-            })
-        } catch (error) {
-            console.error(error)
-        }
-    }
 
     const handleChange = (e: any) => {
         setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
+
+
     const handleSubmit = async (e: any) => {
         e.preventDefault()
         try {
-            await axios.put(`/api/user/${userId}`, formData)
-            toast.success("Profile Updated Successfully")
+            const res = await axios.put(`/api/user/${currentUser?.userId}`, {
+                name: formData.name,
+                email: formData.email,
+                phone: formData.phone,
+                address: formData.address,
+            })
+
+            if (res.data.success === true) {
+                const userData = res.data.user
+                setFormData({
+                    name: userData.name,
+                    email: userData.email,
+                    phone: userData.phone,
+                    address: userData.address,
+                })
+                toast.success("Profile Updated Successfully")
+            }
+
+            if (res.data.success === false) {
+                toast.error("Failed to update profile")
+
+            }
+
         } catch (error) {
             console.log(error)
             toast.error("Failed to update profile")
         }
+
     }
+
 
     const logOut = async () => {
         try {
@@ -65,6 +73,7 @@ const UserProfile = () => {
             console.log(error)
         }
     }
+
 
     if (currentUser?.token && currentUser.isAdmin === true) {
         return router.push('/admin-panel')
@@ -78,7 +87,7 @@ const UserProfile = () => {
         <div className='flex flex-col items-center justify-center gap-3 py-2 px-5'>
             <h1 className='text-[30px] text-td-secondary font-bold'>Profile</h1>
             <div className="flex  md:flex-row flex-col w-full h-full gap-2">
-                <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-3 border px-5 py-8 rounded-2xl lg:w-1/2 w-full bg-slate-50'>
+                <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-3 border px-5 py-8 rounded-2xl lg:w-1/2 w-full bg-slate-100'>
                     <div className='flex items-center justify-center gap-2 w-full'>
                         <RiAccountCircleFill size={30} />
                         <input type="text" name="name" placeholder='Name' value={formData.name} onChange={handleChange} className='border px-5  py-2 rounded-2xl bg-slate-200 w-full' />
@@ -97,7 +106,7 @@ const UserProfile = () => {
                     </div>
                     <button className={`px-5 rounded-2xl py-3 border bg-td-secondary text-white font-bold`} type='submit'>Save</button>
                 </form>
-                <div className='flex items-center justify-center flex-col lg:w-1/2 w-full border px-5 py-8 gap-1 rounded-2xl'>
+                <div className='flex items-center justify-center flex-col lg:w-1/2 w-full border px-5 py-8 gap-1 rounded-2xl bg-slate-100'>
                     <button onClick={() => router.push(`/account/${userId}/wishlist`)} className='px-5 rounded-2xl py-3 border  bg-td-secondary text-white font-bold w-[200px]'>WishList</button>
                     <button onClick={() => router.push(`/account/${userId}/orders`)} className='px-5 rounded-2xl py-3 border bg-td-secondary text-white font-bold w-[200px]'>Orders</button>
                     <button onClick={() => router.push(`/account/${userId}/changePassword`)} className='px-5 rounded-2xl py-3 border  bg-td-secondary text-white font-bold w-[200px]'>Change Password</button>
