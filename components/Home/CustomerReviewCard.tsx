61cb1814e7bcafd7544e72077152ba10473965c7
hadiRazal <hadhirasal22@gmail.com>
2024-03-22 00:34:11 +0530

    changes in review card

--- components/Home/CustomerReviewCard.tsx
+++ components/Home/CustomerReviewCard.tsx
@@ -5,6 +5,7 @@ import { TiTick } from 'react-icons/ti'
 const CustomerReviewCard = ({ item }: any) => {
     return (
         <div className='border flex flex-col items-start justify-start gap-3 py-5 px-4 rounded-2xl min-w-[300px]'>
+            <span className='flex items-center justify-center gap-2'>{item?.reviewer} <span><TiTick size={24} className='text-black bg-green-500 rounded-full' /></span></span>
             <span className='text-start break-all flex items-center justify-start'>
                 <FaStar size={24} className='text-yellow-300' />
                 <FaStar size={24} className='text-yellow-300' />
@@ -12,7 +13,6 @@ const CustomerReviewCard = ({ item }: any) => {
                 <FaStar size={24} className='text-yellow-300' />
                 <FaStar size={24} className='text-yellow-300' />
             </span>
-            <span className='flex items-center justify-center gap-2'>{item?.reviewer} <span><TiTick size={24} className='text-black bg-green-500 rounded-full' /></span></span>
             <p>{item?.review}</p>
         </div>
     )
