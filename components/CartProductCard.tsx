6c33aa19757b4476176a09d6184108cce26c8b05
hadiRazal <hadhirasal22@gmail.com>
2024-03-26 14:42:05 +0530

    updates

--- components/CartProductCard.tsx
+++ components/CartProductCard.tsx
@@ -76,20 +76,25 @@ const CartProductCard = ({ product, cartItemsFetch }: any) => {
             </div>
             <div className='w-full flex flex-col px-2'>
                 <div className=' flex items-center justify-between'>
-                    <p className='text-[18px] font-semibold overflow-hidden overflow-ellipsis'>{truncateText(product?.title || '', 50)}</p>
+                    <p className='text-[15px] font-semibold overflow-hidden overflow-ellipsis'>{truncateText(product?.title || '', 50)}</p>
                     <MdDelete onClick={handleRemove} size={24} className='hover:scale-110 min-w-[40px] cursor-pointer text-red-600  transition-all duration-300 ease-in-out' />
                 </div>
                 <span className='text-[13px]  font-normal'>Size : {product?.size}</span>
-                <span className='text-[13px]  font-normal'>Color : {product?.color}</span>
-                <span className='text-[13px]  font-normal'>Quantity : {product?.quantity}</span>
-                <span className='text-[13px]  font-normal'>Total : {product?.totalPrice}</span>
+                <span className='flex text-[13px] items-center justify-start gap-2 font-normal'>Color :<span style={{ background: product?.color }} className={`relative cursor-pointer h-[13px] w-[13px] rounded-[50%] flex items-center justify-center shadow-lg`}>
+
+                </span>
+                </span>
+                {/* <span className='text-[13px]  font-normal'>Quantity : {product?.quantity}</span> */}
                 <div className='flex items-center justify-between w-full'>
-                    <span className='text-red-600'>&#8377; {product?.price} </span>
+
+                    <span>Quantity</span>
                     <span className='bg-gray-200 flex items-center justify-center gap-4 px-8 py-2 rounded-2xl w-[100px]'>
                         <span className='cursor-pointer' onClick={() => handleQuantity("decrement")}>-</span>
                         <span>{quantity}</span>
                         <span className='cursor-pointer' onClick={() => handleQuantity("increment")}>+</span>
                     </span>
+
+
                 </div>
             </div>
         </div>
