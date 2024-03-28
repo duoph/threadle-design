b8ee1ad3e8122f569ae51cbf5fd7f8bf4a51a0ee
hadiRazal <hadhirasal22@gmail.com>
2024-03-28 14:07:22 +0530

    updates

--- app/api/wishlistFetch/[userId]/route.ts
+++ app/api/wishlistFetch/[userId]/route.ts
@@ -1,27 +1,27 @@
+import ProductModel from "@/models/productModel";
 import userModel from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function GET(req: NextRequest, { params }: any) {
     try {
-
-        const userId = await params.userId;
+        const userId = params.userId;
 
         if (!userId) {
-            console.log("user id not found")
+            console.log("User id not found");
             return NextResponse.json({ message: "User Id not found", success: false });
         }
 
-        const wishList = await userModel.findOne({ _id: userId })
-        const user = await userModel.findOne({ _id: userId }).populate("wishList")
+        const user = await userModel.findOne({ _id: userId }).populate("wishList");
 
         if (!user) {
             return NextResponse.json({ message: "User not found", success: false });
         }
 
-        return NextResponse.json({ message: "Fetched user wishlist", success: true, wishList: wishList?.wishList, user });
+        const wishListItems = await ProductModel.find({ _id: { $in: user.wishList } });
 
-    } catch (error: any) {
+        return NextResponse.json({ message: "Fetched user wishlist", success: true, wishList: user.wishList, user, wishListItems });
+
+    } catch (error:any) {
         console.error('Error while fetching wishlist:', error);
         return NextResponse.json({ message: "Error while fetching wishlist", success: false, error: error.message });
     }
