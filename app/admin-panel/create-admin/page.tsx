6b8bcc61b23b764b370cea7f8f8f8440685aea04
hadiRazal <hadhirasal22@gmail.com>
2024-03-28 23:56:59 +0530

    updates

--- app/admin-panel/create-admin/page.tsx
+++ app/admin-panel/create-admin/page.tsx
@@ -39,6 +39,8 @@ const LoginPageAdmin = () => {
             if (response.data.success === false) {
                 toast.success("Unable to make admin")
             }
+            setIsLoading(false);
+
 
         } catch (error) {
             setIsLoading(false);
@@ -60,10 +62,10 @@ const LoginPageAdmin = () => {
 
 
     return (
-        <div className='bg-td-secondary py-10 flex items-start justify-center px-5 h-[80vh]'>
+        <div className='bg-td-secondary py-[50px] flex items-center justify-center px-5 h-[70vh]'>
             <div className='flex bg-white flex-col gap-5 items-center justify-center w-full md:w-[350px] md:px-10 py-10 px-5 rounded-2xl'>
                 <div>
-                    <h1 className='font-bold text-[30px] text-td-secondary'>Create new admin</h1>
+                    <h1 className='font-bold  text-[22px] sm:text-[30px] text-td-secondary'>Create new admin</h1>
                 </div>
                 <div className='flex flex-col gap-3 w-full'>
                     <input type="email" placeholder='Email' className='border px-5 py-2 rounded-2xl bg-slate-200' value={email} onChange={(e) => setEmail(e.target.value)} />
