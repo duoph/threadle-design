4c7e92d00454bb057b28ad58a6c7481646003d36
hadiRazal <hadhirasal22@gmail.com>
2024-03-29 00:06:25 +0530

    admin creation

--- app/admin-panel/create-admin/page.tsx
+++ app/admin-panel/create-admin/page.tsx
@@ -63,9 +63,9 @@ const LoginPageAdmin = () => {
 
     return (
         <div className='bg-td-secondary py-[50px] flex items-center justify-center px-5 h-[70vh]'>
-            <div className='flex bg-white flex-col gap-5 items-center justify-center w-full md:w-[350px] md:px-10 py-10 px-5 rounded-2xl'>
+            <div className='flex bg-white flex-col gap-5 items-center justify-center w-full md:w-[400px] md:px-10 py-10 px-5 rounded-2xl'>
                 <div>
-                    <h1 className='font-bold  text-[22px] sm:text-[30px] text-td-secondary'>Create new admin</h1>
+                    <h1 className='font-bold  text-[22px] sm:text-[35px]  text-td-secondary'>Create new admin</h1>
                 </div>
                 <div className='flex flex-col gap-3 w-full'>
                     <input type="email" placeholder='Email' className='border px-5 py-2 rounded-2xl bg-slate-200' value={email} onChange={(e) => setEmail(e.target.value)} />
