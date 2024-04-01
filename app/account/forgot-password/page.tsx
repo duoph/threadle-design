6721625de6e84d53af4e98a148da980207622517
hadiRazal <hadhirasal22@gmail.com>
2024-04-02 01:22:19 +0530

    added password change page

--- app/account/forgot-password/page.tsx
+++ app/account/forgot-password/page.tsx
@@ -11,6 +11,9 @@ import { PulseLoader } from 'react-spinners';
 const ForgotPassword = () => {
     const [securityCode, setSecurityCode] = useState<string>('')
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [verified, setVerified] = useState<boolean>(false)
+    const [password, setPassword] = useState<string>('')
+    const [confirmedPassword, setConfirmedPassword] = useState<string>('')
 
     const router = useRouter();
 
@@ -24,41 +27,30 @@ const ForgotPassword = () => {
     }
 
 
+
     const handleVerify = async () => {
-        try {
-            setIsLoading(true);
+        setVerified(true)
 
-            if (!securityCode) {
-                setIsLoading(false);
-                toast.error('Enter valid credentials');
-                return;
-            }
-
-            const response = await axios.post('/api/forgot-password', {
-                securityCode
-            });
-
-
-            if (response.data.success) {
-                toast.success(response.data.message)
-                router.push("/admin-panel")
-            }
-
-            if (response.data.success === false) {
-                toast.error("Unable to make admin")
-            }
-
-
-
-            setIsLoading(false);
-
-
-        } catch (error) {
-            setIsLoading(false);
-            console.error('Error during login:', error);
-            toast.error('Error unable to create admin');
-        }
     };
+
+
+    const handleNewPassword = () => {
+
+        setIsLoading(true)
+        try {
+            if (password !== confirmedPassword) {
+                toast.error("Password dosn't match")
+                setIsLoading(false)
+
+            }
+            if (password === confirmedPassword) {
+                setIsLoading(false)
+                toast.success("Password updated successfully")
+            }
+        } catch (error) {
+
+        }
+    }
 
 
     // if (currentUser?.token && currentUser.isAdmin === true) {
@@ -71,23 +63,50 @@ const ForgotPassword = () => {
 
     return (
         <div className='bg-td-secondary py-[50px] flex items-center justify-center px-5 h-[70vh]'>
-            <div className='flex bg-white flex-col gap-5 items-center justify-center w-full md:w-[400px] md:px-10 py-10 px-5 rounded-2xl'>
-                <div className='flex flex-col items-center justify-center'>
-                    <h1 className='font-bold  text-[22px] sm:text-[35px]  text-td-secondary'>Verify</h1>
-                    <span className='font-bold  text-sm text-gray-500 flex gap-1'>a six digit security code is sent to your email <p onClick={handleResend} className="text-blue-950 cursor-pointer">resend</p></span>
-                </div>
-                <div className='flex flex-col gap-3 w-full'>
-                    <input type="email" placeholder='Enter the verification code' className='border px-5 py-2 rounded-2xl bg-slate-200' value={securityCode} onChange={(e) => setSecurityCode(e.target.value)} />
+            <div className='flex bg-white items-center justify-center w-full md:w-[400px] md:px-10 py-10 px-5 rounded-2xl'>
+                {!verified && (<div className='flex flex-col items-center justify-normal w-full gap-3'>
+                    <div className='flex flex-col items-center justify-center'>
+                        <h1 className='font-bold  text-[22px] sm:text-[35px]  text-td-secondary'>Verify</h1>
+                        <div className='flex font-semibold text-sm gap-1'>
+                            <span className=' text-gray-500 flex flex-wrap'>a six digit security code is sent to your email</span>
 
-                    <button onClick={handleVerify} className='bg-td-secondary rounded-2xl h-12  px-3 py-3 text-white font-semibold'>
-                        {isLoading && (
-                            <PulseLoader color="#ffffff" size={15} />
-                        )}
-                        {!isLoading && (
-                            <span className="text-[15px] w-full">Verify</span>
-                        )}
-                    </button>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-3 w-full'>
+                        <input type="email" placeholder='Enter the verification code' className='border px-5 py-2 rounded-2xl bg-slate-200' value={securityCode} onChange={(e) => setSecurityCode(e.target.value)} />
+
+                        <button onClick={handleVerify} className='bg-td-secondary rounded-2xl h-12  px-3 py-3 text-white font-semibold'>
+                            {isLoading && (
+                                <PulseLoader color="#ffffff" size={15} />
+                            )}
+                            {!isLoading && (
+                                <span className="text-[15px] w-full">Verify</span>
+                            )}
+                        </button>
+                    </div>
+                    <span onClick={handleResend} className="text-sm cursor-pointer font-semibold">didnt recive the code?<span className='text-blue-950'>resend</span></span>
                 </div>
+                )}
+
+                {verified && (<div className='flex flex-col items-center justify-normal w-full gap-3'>
+                    <div className='flex flex-col items-center justify-center'>
+                        <h1 className='font-bold  text-[22px] sm:text-[35px]  text-td-secondary'>New Password</h1>
+                    </div>
+                    <div className='flex flex-col gap-3 w-full'>
+                        <input type="email" placeholder='Enter new password' className='border px-5 py-2 rounded-2xl bg-slate-200' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input type="email" placeholder='confirm the password' className='border px-5 py-2 rounded-2xl bg-slate-200' value={confirmedPassword} onChange={(e) => setConfirmedPassword(e.target.value)} />
+                        <button onClick={handleNewPassword} className='bg-td-secondary rounded-2xl h-12  px-3 py-3 text-white font-semibold'>
+                            {isLoading && (
+                                <PulseLoader color="#ffffff" size={15} />
+                            )}
+                            {!isLoading && (
+                                <span className="text-[15px] w-full">Change password</span>
+                            )}
+                        </button>
+                    </div>
+                    {/* <span onClick={handleResend} className="text-sm cursor-pointer font-semibold">didnt recive the code?<span className='text-blue-950'>resend</span></span> */}
+                </div>
+                )}
             </div>
         </div>
     );
--- app/api/forgot-password/route.ts
+++ app/api/forgot-password/route.ts
@@ -3,28 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 import userModel from "@/models/userModel";
 
 function generateRandomCode(): string {
-    const min = 100000; // Minimum 6-digit number
-    const max = 999999; // Maximum 6-digit number
-    return Math.floor(Math.random() * (max - min + 1)) + min + ''; // Convert to string
+    const min = 100000; 
+    const max = 999999; 
+    return Math.floor(Math.random() * (max - min + 1)) + min + ''; 
 }
 
 export async function POST(req: NextRequest) {
     try {
-        const userId = getDataFromToken(req); // Assuming this retrieves user ID from token
-        const user = await userModel.findById(userId); // Assuming you have a User model and findById function
+        const userId = getDataFromToken(req); 
+        const user = await userModel.findById(userId); 
 
         if (!user) {
             return NextResponse.json({ message: "User not found" }, { status: 404 });
         }
 
-        // Generate a unique security code
-        const securityCode = generateRandomCode(); // Generate a 6-digit random code
-
-        // Save the security code in the user document in MongoDB
+        const securityCode = generateRandomCode();
         user.securityCode = securityCode;
         await user.save();
 
-        // Send the security code to the user via email or SMS (not implemented here)
 
         return NextResponse.json({ message: "Security code generated and sent to your email" });
 
