82e87077dc698a1b133f9fa79b18115c8a61fd21
hadiRazal <hadhirasal22@gmail.com>
2024-04-05 02:04:06 +0530

    order manage page

--- app/admin-panel/orders/page.tsx
+++ app/admin-panel/orders/page.tsx
@@ -1,31 +1,67 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 const Orders = () => {
+
+    const [selectedOrderType, setSelectedOrderType] = useState<string>("pending")
+
     return (
-        <div className='flex flex-col justify-center items-center py-5 gap-5'>
-            <h1 className='text-td-secondary font-bold text-[30px]'>Order Dashboard</h1>
-            <div className=' h-[30vh] flex  items-center justify-center'>
-                <div className='flex flex-col md:flex-row gap-3 text-td-secondary font-bold text-[20px]'>
+        <div className='flex flex-col items-center py-5 px-3 gap-3'>
+            <h1 className='text-td-secondary font-bold text-3xl'>Order Dashboard</h1>
+            <div className='flex items-center justify-center border rounded-2xl py-5 px-3 w-full'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-3 text-td-secondary font-bold text-xl'>
                     <div className='flex flex-col items-center justify-center'>
-                        <h1 className=''>Total Orders</h1>
+                        <h2>Total Orders</h2>
                         <span>99</span>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h1>Paid Orders</h1>
+                        <h2>Paid Orders</h2>
                         <span>99</span>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h1>Shipped Orders</h1>
+                        <h2>Shipped Orders</h2>
                         <span>99</span>
                     </div>
                 </div>
             </div>
-            <div className='bg-green-500 h-[30vh]'>
 
+            <div className='flex items-center  justify-center gap-2 md:gap-5 lg:gap-10 border rounded-2xl py-5 px-5 md:px-10 w-full text-[15px] flex-wrap'>
+                <span
+                    onClick={() => setSelectedOrderType('pending')}
+                    className={`px-2 py-2 rounded-2xl cursor-pointer border ${selectedOrderType === 'pending' ? 'bg-td-secondary text-white' : ''}`}
+                >
+                    Pending Orders
+                </span>
+                <span
+                    onClick={() => setSelectedOrderType('shipped')}
+                    className={`px-3 py-2 rounded-2xl cursor-pointer border ${selectedOrderType === 'shipped' ? 'bg-td-secondary text-white' : ''}`}
+                >
+                    Shipped Orders
+                </span>
+                <span
+                    onClick={() => setSelectedOrderType('delivered')}
+                    className={`px-3 py-2 rounded-2xl cursor-pointer border ${selectedOrderType === 'delivered' ? 'bg-td-secondary text-white' : ''}`}
+                >
+                    Delivered Orders
+                </span>
             </div>
-
+            <div className='flex flex-col border rounded-2xl py-5 px-3 w-full gap-[10px] '>
+                <div className='flex items-center justify-between border-b-2 px-2'>
+                    <span>Product Name</span>
+                    <span>Customer Name</span>
+                </div>
+                <div className='flex items-center justify-between border rounded-2xl overflow-hidden pr-3'>
+                    <div className='flex items-center gap-1 w-2/3'>
+                        <Image src={"/noImage.jpg"} alt='no Image' width={100} height={100} />
+                        <span>This is thererwe best</span>
+                    </div>
+                    <span >Hadi Rasal</span>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Orders
+export default Orders;
