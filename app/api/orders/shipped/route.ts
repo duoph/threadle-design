5e707b5516746e37f7f3fc5eadbe8e0c1e8bd34e
hadiRazal <hadhirasal22@gmail.com>
2024-04-10 22:00:14 +0530

    updates

--- app/api/orders/shipped/route.ts
+++ app/api/orders/shipped/route.ts
@@ -1,5 +1,6 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
 import CartModel from "@/models/cartItemModel";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
@@ -8,5 +9,27 @@ export async function GET() {
     } catch (error) {
         console.error(error);
         return NextResponse.json(new Error("Error in fetching the delivered orders"));
+    }
+}
+
+export async function PUT(req: NextRequest) {
+    try {
+        const { userId } = await getDataFromToken(req);
+
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = await req.json()
+
+        console.log(razorpay_order_id, razorpay_payment_id, razorpay_signature)
+
+        if (!userId) {
+            return NextResponse.json({ message: "Unauthenticated Access Error while fetching the cart items", success: false });
+        }
+
+        const cartItems = await CartModel.updateMany({ userId }, { isShipped: true });
+
+        return NextResponse.json({ message: "Marked all cart items as paid", success: true, cartItems });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Error while marking all cart items as paid", success: false, error });
     }
 }
