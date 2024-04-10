b94b2e36b7ac5fa20db60ed7b714caa4795ab165
Hadi Razal <hadhirasal22@gmail.com>
2024-04-10 18:57:40 +0530

    Update route.ts
    Add shipped api update option

--- app/api/orders/shipped/route.ts
+++ app/api/orders/shipped/route.ts
@@ -10,3 +10,28 @@ export async function GET() {
         return NextResponse.json(new Error("Error in fetching the delivered orders"));
     }
 }
+ 
+
+
+export async function PUT(req: NextRequest) {
+    try {
+        const { userId } = await getDataFromToken(req);
+
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = await req.json()
+
+        console.log(razorpay_order_id, razorpay_payment_id, razorpay_signature)
+
+        if (!userId) {
+            return NextResponse.json({ message: "Unauthenticated Access Error while fetching the cart items", success: false });
+        }
+
+        const cartItems = await CartModel.updateMany({ userId }, { isShipped: true);
+
+        return NextResponse.json({ message: "Marked all cart items as paid", success: true, cartItems });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Error while marking all cart items as paid", success: false, error });
+    }
+}
+
