36876d40dafd46f63621cf31c698be127ffc655b
hadiRazal <hadhirasal22@gmail.com>
2024-04-13 23:56:14 +0530

    is delivered api update

--- app/api/orders/delivered/route.ts
+++ app/api/orders/delivered/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
     }
 }
 
-export async function PUT(req: NextRequest) {
+export async function POST(req: NextRequest) {
     try {
 
         const { productId } = await req.json()
@@ -25,6 +25,26 @@ export async function PUT(req: NextRequest) {
         const cartItems = await CartModel.updateMany({ productId: productId }, { isDelivered: true });
 
         return NextResponse.json({ message: "Marked all cart items as paid", success: true, cartItems });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Error while marking all cart items as paid", success: false, error });
+    }
+}
+
+
+export async function PUT(req: NextRequest) {
+    try {
+
+        const { cartId } = await req.json()
+
+        if (!cartId) {
+            return NextResponse.json({ message: "Unauthenticated Access Error while fetching the cart items", success: false });
+        }
+
+        const cartItem = await CartModel.findByIdAndUpdate({ _id: cartId }, { isDelivered: false });
+
+        return NextResponse.json({ message: "Marked all cart items as paid", success: true, cartItem });
 
     } catch (error) {
         console.log(error);
