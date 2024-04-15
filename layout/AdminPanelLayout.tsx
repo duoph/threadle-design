73834a686d4a4b452f9d234e2435f2ab55ecaa89
hadiRazal <hadhirasal22@gmail.com>
2024-04-15 12:21:09 +0530

    made changes in layout

--- layout/AdminPanelLayout.tsx
+++ layout/AdminPanelLayout.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import React, { ReactNode, useState } from 'react';
 import ClickAwayListener from 'react-click-away-listener';
+import { AiOutlineLogout } from 'react-icons/ai';
 import { IoMdClose } from 'react-icons/io';
 import { IoMenu } from 'react-icons/io5';
 
@@ -16,6 +17,7 @@ const AdminPanelLayout = ({ children }: { children: ReactNode }) => {
             <div className='text-td-secondary px-6 py-5'>
                 <button className=' rounded-2xl' onClick={() => setIsMenu(true)}>
                     <IoMenu className='cursor-pointer' size={24} />
+
                 </button>
             </div>
 
@@ -25,18 +27,20 @@ const AdminPanelLayout = ({ children }: { children: ReactNode }) => {
 
             {isMenu && (
                 <ClickAwayListener onClickAway={() => setIsMenu(false)}>
-                    <div className={`fixed top-5 left-0 md:flex items-start justify-center bg-slate-200 w-[300px] z-[99] h-full  transition-transform duration-300 ease-in-out ${isMenu ? 'translate-x-0' : '-translate-x-[100%]'}`}>
+                    <div className={`fixed top-5 left-0 md:flex items-center justify-center bg-slate-200 w-[300px] z-[99] h-full  transition-transform duration-300 ease-in-out ${isMenu ? 'translate-x-0' : '-translate-x-[100%]'}`}>
                         <div className='absolute top-2 right-2 cursor-pointer' onClick={() => setIsMenu(false)}>
                             <IoMdClose className='text-white cursor-pointer' size={24} />
                         </div>
-                        <div className="flex flex-col gap-2 items-center justify-center pt-8 ">
-                            <div className='py-10 flex flex-col items-center justify-center gap-[4px] w-full'>
+                        <h1 className='text-center py-2'>Admin Panel</h1>
+                        <div className="flex flex-col gap-2 items-center justify-center ">
+                            <div className=' flex flex-col items-center justify-center gap-[4px] w-full'>
                                 <Link href="/admin-panel/create-product" className='bg-td-secondary w-full px-10 py-2 text-white text-center'>Add Product</Link>
                                 <Link href="/admin-panel/create-category" className='bg-td-secondary w-full px-10 py-2 text-white text-center'>Add Category</Link>
                                 <Link href="/admin-panel/view-products" className='bg-td-secondary w-full px-10 py-2 text-white text-center'>View All Product</Link>
                                 <Link href="/admin-panel/view-categories" className='bg-td-secondary w-full px-10 py-2 text-white text-center'>View All Category</Link>
                                 <Link href="/admin-panel/create-admin" className='bg-td-secondary w-full px-10 py-2 text-white text-center'>Create New Admin</Link>
-                                <button className='bg-red-600 w-full px-10 py-2 text-white text-center' onClick={() => { }}>Logout</button>
+                                <button className='bg-red-600 w-full px-10 py-2 text-white text-center flex items-center justify-center gap-3' onClick={() => { }}> LogOut<AiOutlineLogout />
+                                </button>
                             </div>
                         </div>
                     </div>
