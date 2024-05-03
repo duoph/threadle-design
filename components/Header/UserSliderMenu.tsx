5c31c4b0a57ac7a55150f139ee765ebbeb53dce7
hadiRazal <hadhirasal22@gmail.com>
2024-05-03 18:57:39 +0530

    updates

--- components/Header/UserSliderMenu.tsx
+++ components/Header/UserSliderMenu.tsx
@@ -45,7 +45,7 @@ const UserSliderMenu = () => {
 
             <ClickAwayListener onClickAway={() => setIsMenuUser(true)}>
                 <div onClick={() => setIsMenuUser(true)} className={`fixed top-[81px] h-full right-0 flex flex-col items-start justify-start z-50 shadow-2xl  bg-td-secondary md:w-[300px] w-full  translate-x-[0%]  transition-all duration-300 ease-in-out ${isMenuUser && 'translate-x-[100%]'}`}>
-                    <Link href={`/account/${currentUser?.userId}`} className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith("/admin-panel/create-product") && "bg-td-primary"}`}>
+                    <Link href={`/account/${currentUser?.userId}`} className={` w-full px-10 py-2 text-white  text-center ${pathname === `/account/${currentUser?.userId}` && "bg-td-primary"}`}>
                         <span className="flex items-center justify-center gap-2">
                             <FaUserCircle />
                             <p>
@@ -53,7 +53,7 @@ const UserSliderMenu = () => {
                             </p>
                         </span>
                     </Link>
-                    <Link href="/" className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith("/admin-panel/create-product") && "bg-td-primary"}`}>
+                    <Link href="/" className={` w-full px-10 py-2 text-white text-center ${pathname?.startsWith(`/category/`) && "bg-td-primary"}`}>
                         <span className="flex items-center justify-center gap-2">
                             <CiShop />
                             <p>
@@ -61,24 +61,24 @@ const UserSliderMenu = () => {
                             </p>
                         </span>
                     </Link>
-                    <Link href={`/account/${currentUser?.userId}/wishlist`} className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith("/admin-panel/create-product") && "bg-td-primary"}`}>
-                        <span className="flex items-center justify-center gap-2">
-                            <CiHeart />
-                            <p>
-                                Wishlist
-                            </p>
-                        </span>
-                    </Link>
-                    <Link href={`/account/${currentUser?.userId}/orders`} className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith("/admin-panel/create-product") && "bg-td-primary"}`}>
-                        <span className="flex items-center justify-center gap-2">
-                            <CiShoppingCart />
-                            <p>
-                                My Orders
-                            </p>
-                        </span>
-                    </Link>
-                    <button className='bg-red-600 w-full px-10 py-2 text-white text-center flex items-center justify-center gap-3' onClick={handleLogout}> LogOut<AiOutlineLogout /></button>
-                </div>
+                    <Link href={`/account/${currentUser?.userId}/wishlist`} className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith(`/account/${currentUser?.userId}/wishlist`) && "bg-td-primary"}`}>
+                    <span className="flex items-center justify-center gap-2">
+                        <CiHeart />
+                        <p>
+                            Wishlist
+                        </p>
+                    </span>
+                </Link>
+                <Link href={`/account/${currentUser?.userId}/orders`} className={` w-full px-10 py-2 text-white  text-center ${pathname?.startsWith(`/account/${currentUser?.userId}/orders`) && "bg-td-primary"}`}>
+                    <span className="flex items-center justify-center gap-2">
+                        <CiShoppingCart />
+                        <p>
+                            My Orders
+                        </p>
+                    </span>
+                </Link>
+                <button className='bg-red-600 w-full px-10 py-2 text-white text-center flex items-center justify-center gap-3' onClick={handleLogout}> LogOut<AiOutlineLogout /></button>
+        </div>
             </ClickAwayListener >
         </div >
     )
