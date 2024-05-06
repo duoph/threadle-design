9eac4961305200f8b79999aac0e100904cbecbe2
hadiRazal <hadhirasal22@gmail.com>
2024-05-06 19:15:18 +0530

    updates

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -14,7 +14,6 @@ const Hero = () => {
             <div className='md:w-1/2 w-full flex flex-col items-center justify-center md:py-10 pt-10 lg:px-10 px-5 gap-4 lg:gap-10'>
                 <div>
                     <h1 className='text-center font-extrabold text-td-secondary lg:text-[80px] text-[60px] leading-[60px] pb-6'>
-
                         Threadle Designs
                     </h1>
                     <p className='text-black font-light break-all text-center px-4 pb-2'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
@@ -23,17 +22,16 @@ const Hero = () => {
                     <button onClick={() => router.push('/shop')} className='py-3 px-4 rounded-2xl bg-td-secondary text-[18px] text-white font-semibold md:hover:scale-105 transition-all ease-in-out duration-300' >Shop Now</button>
                 </div>
                 <div className='hidden lg:flex items-center justify-center gap-5 lg:text-[28px] md:text-[20px] text-[16px]'>
-
-                    <p className='flex flex-col items-center justify-center font-semibold '>2000+ <span className='font-light'>Products</span></p>
+                    <p className='flex flex-col items-center justify-center font-semibold '>2000+<span className='font-light'>Products</span></p>
                     <span className='text-gray-600'>|</span>
-                    <p className='flex flex-col items-center justify-center font-semibold '>10000+ <span className='font-light'>Customers</span></p>
+                    <p className='flex flex-col items-center justify-center font-semibold '>10000+<span className='font-light'>Customers</span></p>
                 </div>
             </div>
             <div className='relative md:w-1/2 w-full flex flex-col gap-8 pb-10 items-center justify-center'>
-                <div className='relative'>
-                    <Image priority={true} src={'/stars.svg'} className='absolute bottom-12 right-2' height={40} width={40} alt='Star' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+                <div className='relative flex items-center justify-center'>
+                    <Image priority={true} src={'/stars.svg'} className='absolute bottom-12 md:right-2 right-5' height={40} width={40} alt='Star' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                     <Image priority={true} src={'/stars.svg'} className='absolute top-12 left-20 ' height={40} width={40} alt='Star' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
-                    <div className='relative w-[460px] min-h-[600px]'>
+                    <div className='relative w-[430px] min-h-[600px]'>
                         <Image priority={true} src={"/greendress.png"} fill={true} alt='GreenDress' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                     </div>
                 </div>
