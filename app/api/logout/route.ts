117aaf08e5ec9e4c9d0712dde7efb6878e0877d2
hadiRazal <hadhirasal22@gmail.com>
2024-05-11 09:11:23 +0530

    updates in logout

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,16 +3,15 @@ import { NextResponse } from "next/server";
 export async function GET() {
 
     try {
-        const response = NextResponse.json({ message: "LogOut successful", success: true })
+        const response =  NextResponse.json( { message: "LogOut successful", success: true });
 
-        response.cookies.set("token", "", { expires: new Date(0) })
-        response.cookies.set("isAdmin", "", { expires: new Date(0) })
+        response.cookies.set("token", "", { expires: new Date(0) });
+        response.cookies.set("isAdmin", "", { expires: new Date(0) });
 
-        return response
+        return response;
 
     } catch (error: any) {
-        console.log(error)
-        return NextResponse.json({ message: error.message, success: false })
+        console.log(error);
+        return  NextResponse.json( {message: error.message, success: false });
     }
 }
-
