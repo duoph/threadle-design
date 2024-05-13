5e658d7498499121cb0109d64db2e963d87eade0
hadiRazal <hadhirasal22@gmail.com>
2024-05-13 12:31:49 +0530

    update in header

--- components/OrderDisplayCardUser.tsx
+++ components/OrderDisplayCardUser.tsx
@@ -2,28 +2,19 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { CiCircleCheck, CiClock1 } from 'react-icons/ci';
 import { IoMdCloseCircleOutline } from 'react-icons/io';
 import { formatDistanceToNow } from 'date-fns';
 
 const OrderDisplayCardUser = ({ order }: any) => {
-    const router = useRouter();
-
-    const handleRoute = () => {
-        try {
-            router.push(`/account/${order.userId}/orders/${order._id}`);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
+   
 
     const formattedDate = order?.orderedDate ? formatDistanceToNow(new Date(order?.orderedDate)) : '';
 
 
-
     return (
-        <div key={order._id} onClick={handleRoute} className=' cursor-pointer flex items-center justify-between border rounded-md overflow-hidden pr-3 h-[60px]'>
+        <Link href={`/account/${order.userId}/orders/${order._id}`} className=' cursor-pointer flex items-center justify-between border rounded-md overflow-hidden pr-3 h-[60px]'>
             <div className='flex items-center gap-1 w-2/3 pr-4'>
                 <Image style={{ objectFit: 'cover' }} src={order.imageURL || "/noImage.jpg"} alt='no Image' width={50} height={50} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" priority={true} />
                 <div className='flex flex-col w-full'>
@@ -37,7 +28,7 @@ const OrderDisplayCardUser = ({ order }: any) => {
                 {order.delivered && order.isShipped && !order.isCancel && (<span className='flex flex-col items-center justify-center'>Delivered<CiCircleCheck size={24} /></span>)}
                 {order.isCancel && (<span className='flex flex-col items-center justify-center text-red-700'>Cancelled <IoMdCloseCircleOutline size={24} /></span>)}
             </span>
-        </div>
+        </Link>
     );
 };
 
