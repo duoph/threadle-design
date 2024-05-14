c240a2490c1e739400a0db93c728ccb5b1ecf34b
hadiRazal <hadhirasal22@gmail.com>
2024-05-14 11:57:09 +0530

    updates in admin orders page

--- app/admin-panel/orders/page.tsx
+++ app/admin-panel/orders/page.tsx
@@ -4,18 +4,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import OrderDisplayCard from '@/components/OrderDisplayCard';
-import { Cart, Product } from '@/types';
+import { Cart } from '@/types';
 import { PulseLoader } from 'react-spinners';
 import { CiSearch } from 'react-icons/ci';
 
 const Orders = () => {
     const [selectedOrderType, setSelectedOrderType] = useState<string>('pending');
-    const [pendingOrders, setPendingOrders] = useState<Cart[]>([]);
-    const [shippedOrders, setShippedOrders] = useState<Cart[]>([]);
-    const [deliveredOrders, setDeliveredOrders] = useState<Cart[]>([]);
-    const [cancelOrders, setCancelOrders] = useState<Cart[]>([]);
-    const [orderDisplay, setOrderDisplay] = useState<Cart[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [orders, setOrders] = useState<Cart[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true); // Initially set loading to true
     const [search, setSearch] = useState<string>('');
 
     const router = useRouter();
@@ -24,33 +20,8 @@ const Orders = () => {
         try {
             setIsLoading(true);
 
-            const pendingRes = await axios.get('/api/orders/pending');
-            const shippedRes = await axios.get('/api/orders/shipped');
-            const deliveredRes = await axios.get('/api/orders/delivered');
-            const cancelRes = await axios.get('/api/orders/cancel');
-
-            setPendingOrders(pendingRes.data?.pendingOrders || []);
-            setShippedOrders(shippedRes.data?.shippedOrders || []);
-            setDeliveredOrders(deliveredRes.data?.deliveredOrders || []);
-            setCancelOrders(cancelRes.data?.cancelOrders || []);
-
-            switch (selectedOrderType) {
-                case 'pending':
-                    setOrderDisplay(pendingRes.data?.pendingOrders || []);
-                    break;
-                case 'shipped':
-                    setOrderDisplay(shippedRes.data?.shippedOrders || []);
-                    break;
-                case 'delivered':
-                    setOrderDisplay(deliveredRes.data?.deliveredOrders || []);
-                    break;
-                case 'cancel':
-                    setOrderDisplay(cancelRes.data?.cancelOrders || []);
-                    break;
-                default:
-                    break;
-            }
-
+            const response = await axios.get(`/api/orders/${selectedOrderType}`);
+            setOrders(response.data[selectedOrderType + 'Orders'] || []);
             setIsLoading(false);
         } catch (error) {
             setIsLoading(false);
@@ -59,67 +30,41 @@ const Orders = () => {
     };
 
     useEffect(() => {
-        fetchOrders(); // Fetch orders initially
-        const intervalId = setInterval(fetchOrders, 60000); // Fetch orders every 5 seconds
+        fetchOrders();
+    }, [selectedOrderType]);
 
-        return () => clearInterval(intervalId); // Cleanup interval on unmount
-    }, [selectedOrderType]); // Fetch orders when selectedOrderType changes
-
-    useEffect(() => {
-        const filtered = orderDisplay.filter((order: Cart) =>
-            order.customerName?.toLowerCase().includes(search.toLowerCase()) ||
-            order.title?.toLowerCase().includes(search.toLowerCase()) || order.phoneNumber?.toLowerCase().includes(search.toLowerCase()) || order.whatsAppNumber?.toLowerCase().includes(search.toLowerCase()) || order.razorpay_order_id?.toLowerCase().includes(search.toLowerCase())
-        );
-        setOrderDisplay(filtered);
-    }, [search, orderDisplay]);
-
-    if (isLoading && shippedOrders.length === 0 && pendingOrders.length === 0 && deliveredOrders.length === 0) {
-        return (
-            <div className='flex flex-col items-center py-5 px-3 gap-3 min-h-[85vh]'>
-                <h1 className='text-td-secondary text-center text-[25px] md:text-[35px] font-bold text-3xl'>Order Dashboard</h1>
-                <div className=" absolute flex items-center justify-center flex-grow h-[65vh]">
-                    <PulseLoader />
-                </div>
-            </div>
-        );
-    }
+    const filteredOrders = orders.filter(order =>
+        order.customerName?.toLowerCase().includes(search.toLowerCase()) ||
+        order.title?.toLowerCase().includes(search.toLowerCase()) ||
+        order.phoneNumber?.toLowerCase().includes(search.toLowerCase()) ||
+        order.whatsAppNumber?.toLowerCase().includes(search.toLowerCase()) ||
+        order.razorpay_order_id?.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <div className="flex flex-col items-center py-5 px-3 gap-2 w-full">
             <h1 className='text-td-secondary text-center text-[25px] md:text-[35px] font-bold text-3xl'>Order Dashboard</h1>
             <div className="flex items-center justify-center gap-2 md:gap-5 lg:gap-10 rounded-md py-5 px-5 md:px-10 w-full text-[15px] flex-wrap">
                 <span
-                    onClick={() => {
-                        setSelectedOrderType('pending');
-                        setOrderDisplay(pendingOrders);
-                    }}
+                    onClick={() => setSelectedOrderType('pending')}
                     className={`px-2 py-2 rounded-md cursor-pointer border ${selectedOrderType === 'pending' ? 'bg-td-secondary text-white' : ''}`}
                 >
                     Pending Orders
                 </span>
                 <span
-                    onClick={() => {
-                        setSelectedOrderType('shipped');
-                        setOrderDisplay(shippedOrders);
-                    }}
+                    onClick={() => setSelectedOrderType('shipped')}
                     className={`px-3 py-2 rounded-md cursor-pointer border ${selectedOrderType === 'shipped' ? 'bg-td-secondary text-white' : ''}`}
                 >
                     Shipped Orders
                 </span>
                 <span
-                    onClick={() => {
-                        setSelectedOrderType('delivered');
-                        setOrderDisplay(deliveredOrders);
-                    }}
+                    onClick={() => setSelectedOrderType('delivered')}
                     className={`px-3 py-2 rounded-md cursor-pointer border ${selectedOrderType === 'delivered' ? 'bg-td-secondary text-white' : ''}`}
                 >
                     Delivered Orders
                 </span>
                 <span
-                    onClick={() => {
-                        setSelectedOrderType('cancel');
-                        setOrderDisplay(cancelOrders);
-                    }}
+                    onClick={() => setSelectedOrderType('cancel')}
                     className={`px-3 py-2 rounded-md cursor-pointer border ${selectedOrderType === 'cancel' ? 'bg-td-secondary text-white' : ''}`}
                 >
                     Cancelled Orders
@@ -137,19 +82,32 @@ const Orders = () => {
                 <CiSearch className='rounded-md text-[30px] cursor-pointer text-white' />
             </div>
 
-            <div className="flex flex-col border rounded-md py-5 px-3 w-full gap-[10px] min-h-[70vh]">
-                <div className="flex items-center justify-between border-b-2 px-2">
-                    <span className="w-2/6 text-center">Product Name</span>
-                    <span className="w-2/6 text-center">Customer</span>
+            {isLoading ? (
+                <div className="flex flex-col border rounded-md py-5 px-3 w-full gap-[10px] min-h-[70vh]">
+                    <div className="flex items-center justify-between border-b-2 px-2">
+                        <span className="w-2/6 text-center">Product Name</span>
+                        <span className="w-2/6 text-center">Customer</span>
+                    </div>
+                    <div className='flex flex-col items-center gap-3 py-10 min-h-[85vh]'>
+                        <PulseLoader />
+                    </div>
                 </div>
-                {orderDisplay?.length === 0 ? (
-                    <div className="flex items-center justify-center w-full h-full">No Orders Available</div>
-                ) : (
-                    orderDisplay.map((order, i) => (
+
+            ) : (
+                <div className="flex flex-col border rounded-md py-5 px-3 w-full gap-[10px] min-h-[70vh]">
+                    <div className="flex items-center justify-between border-b-2 px-2">
+                        <span className="w-2/6 text-center">Product Name</span>
+                        <span className="w-2/6 text-center">Customer</span>
+                    </div>
+                    {filteredOrders.length === 0 && (
+                        <div className="flex items-center justify-center w-full h-full">No Orders Available</div>
+                    )}
+
+                    {filteredOrders.map((order, i) => (
                         <OrderDisplayCard key={i} order={order} />
-                    ))
-                )}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
