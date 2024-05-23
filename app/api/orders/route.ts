ee79775e8f1e5676e94218cadcc13b92b9c244d1
hadiRazal <hadhirasal22@gmail.com>
2024-05-23 22:04:03 +0530

    updates

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -3,13 +3,16 @@ import CartModel from "@/models/cartItemModel";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-
     try {
-        connectMongoDB()
-        const orders = await CartModel.find()
-        return NextResponse.json({ message: " fetched all orders", orders });
+        await connectMongoDB();
+        const orders = await CartModel.find();
+        console.log('Orders fetched from DB:', orders); // Logging for debugging
+
+        const response = NextResponse.json({ message: "Fetched all orders", orders });
+        response.headers.set('Cache-Control', 'no-store'); // Disable caching
+        return response;
     } catch (error) {
-        console.error(error);
+        console.error('Error in fetching all orders:', error);
         return NextResponse.json(new Error("Error in fetching all orders"));
     }
 }
