6f21dcd4f586cd4e59efe5ff2db187a2df010a7f
hadiRazal <hadhirasal22@gmail.com>
2024-05-24 00:42:16 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
         domains: ["threadle-designs01.s3.amazonaws.com", "threadle-designs.s3.amazonaws.com"]
-    }
+    }, headers: [
+        {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+        },
+    ],
 };
 
 export default nextConfig;
