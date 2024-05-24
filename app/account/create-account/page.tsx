c259b6c8450cdbc0d7829f41db2636a11efd1c8a
hadiRazal <hadhirasal22@gmail.com>
2024-05-24 23:38:19 +0530

    login and create account page design changed

--- app/account/create-account/page.tsx
+++ app/account/create-account/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
@@ -18,11 +18,8 @@ interface FormData {
 }
 
 const CreateAccount = () => {
-
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [confirmPass, setConfirmPass] = useState<string>()
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [showWhatsApp, setShowWhatsApp] = useState<boolean>(false);
-
 
     const [formData, setFormData] = useState<FormData>({
         phone: '',
@@ -43,18 +40,17 @@ const CreateAccount = () => {
         });
     };
 
-    const isMatchCheck = (e: ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        setConfirmPass(value);
-    };
-
     const passwordsMatch = () => {
-        return formData.password === confirmPass;
+        return formData.password === formData.confirmPassword;
     };
-
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
+
+        if (!formData.name || !formData.phone || !formData.email || !formData.password || (showWhatsApp && !formData.whatsApp)) {
+            toast.error("All fields are required");
+            return;
+        }
 
         if (!passwordsMatch()) {
             toast.error("Passwords do not match");
@@ -62,124 +58,91 @@ const CreateAccount = () => {
         }
 
         try {
-            setIsLoading(true)
+            setIsLoading(true);
 
             const formDataToSend = new FormData();
             formDataToSend.append('name', formData.name);
             formDataToSend.append('phone', formData.phone);
             formDataToSend.append('email', formData.email);
             formDataToSend.append('password', formData.password);
-            showWhatsApp ? formDataToSend.append('whatsApp', formData.whatsApp) : formDataToSend.append('whatsApp', formData.phone)
-
-
-            if (formData.name === "" || formData.phone === "" || formData.password === "") {
-                toast.error("Unable to create account")
-                setIsLoading(false)
-            }
+            formDataToSend.append('whatsApp', showWhatsApp ? formData.whatsApp : formData.phone);
 
             const response = await axios.post('/api/create-account', formDataToSend);
 
-
-            if (response.data.success === true) {
-                toast.success("Account created successfully")
-                router.push(`/account/login`)
-                setIsLoading(false)
+            if (response.data.success) {
+                toast.success("Account created successfully");
+                router.push(`/account/login`);
+            } else {
+                toast.error(response.data.message);
+                console.log(response.data);
             }
-
-            if (response.data.success === false) {
-                toast.error(response.data.message)
-                console.log(response.data)
-            }
-
-
-            setIsLoading(false)
         } catch (error) {
-            setIsLoading(false)
-            toast.error("Unable to create account")
+            toast.error("Unable to create account");
             console.error('Error submitting form:', error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
-
     return (
-        <div className='flex flex-col items-center justify-center bg-td-secondary gap-3 py-5 md:px-10 px-5 min-h-[85vh]'>
-
+        <div className='flex flex-col items-center justify-center gap-3 py-5 md:px-10 px-5 min-h-[90vh]'>
             <div className='flex items-center justify-center pb-10 w-full'>
-
-                <form onSubmit={handleSubmit} className='rounded-md bg-white flex flex-col gap-3 md:w-[400px] w-full md:px-10 py-8 px-5 '>
-                    <div className='h-[10vh] flex flex-col items-center justify-start'>
+                <form onSubmit={handleSubmit} className='rounded-md bg-white flex flex-col gap-3 md:w-[400px] w-full md:px-10 py-8 px-5'>
+                    <div className='flex flex-col items-center justify-start'>
                         <h1 className='text-center font-bold text-td-secondary text-[30px]'>Create Account</h1>
-                        {!passwordsMatch() && confirmPass && <span className='text-red-600 text-center transition-all ease-in-out duration-300 '>Password doesn&#39;t match</span>}
+                        {!passwordsMatch() && formData.confirmPassword && <span className='text-red-600 text-center transition-all ease-in-out duration-300'>Passwords do not match</span>}
                     </div>
-
-                    <div className='flex items-center justify-center gap-2 '>
-                        <FaUserCircle size={24} />
+                    <div className='flex items-center justify-center gap-2 rounded-md border px-2'>
                         <input
                             type="text"
                             id="name"
                             name="name"
                             value={formData.name}
                             onChange={handleChange}
-                            placeholder='Name' className='border px-5 py-2 rounded-md bg-slate-200 w-full'
+                            placeholder='Name'
+                            className='w-full px-3 py-3 focus:outline-none'
                         />
                     </div>
-
-
-                    <div className='flex items-center justify-center gap-2'>
-                        <FaPhone size={24} />
-                        <div className='flex items-center justify-center rounded-md bg-slate-200 w-full'>
-                            <span className='pl-2'>+91</span>
-                            <input
-                                type="string"
-                                // pattern='0-9'
-                                id="phone"
-                                name="phone"
-                                maxLength={10}
-                                value={formData.phone}
-                                onChange={handleChange}
-                                placeholder='Phone'
-                                className='border px-3 py-2 rounded-md bg-slate-200 w-full'
-                            />
-
-                        </div>
-
+                    <div className='flex items-center justify-center gap-2 rounded-md border px-2'>
+                        <span className=''>+91</span>
+                        <input
+                            type="text"
+                            id="phone"
+                            name="phone"
+                            maxLength={10}
+                            value={formData.phone}
+                            onChange={handleChange}
+                            placeholder='Phone'
+                            className='w-full px-3 py-3 focus:outline-none'
+                        />
                     </div>
-
-
                     {showWhatsApp && (
-                        <div className='flex items-center justify-center gap-2'>
-                            <FaSquareWhatsapp size={24} />
-                            <div className='flex items-center justify-center rounded-md bg-slate-200 w-full'>
-                                <span className='pl-2'>+91</span>
-                                <input
-                                    type="number"
-                                    id="whatsApp"
-                                    name="whatsApp"
-                                    maxLength={10}
-                                    value={formData.whatsApp}
-                                    onChange={handleChange}
-                                    placeholder='whatsApp' className='border px-3 py-2 rounded-md bg-slate-200 w-full'
-                                />
-
-                            </div>
+                        <div className='flex items-center justify-center gap-2 rounded-md border px-2'>
+                            <span className=''>+91</span>
+                            <input
+                                type="text"
+                                id="whatsApp"
+                                name="whatsApp"
+                                maxLength={10}
+                                value={formData.whatsApp}
+                                onChange={handleChange}
+                                placeholder='WhatsApp'
+                                className='w-full px-3 py-3 focus:outline-none'
+                            />
                         </div>
                     )}
-
-
-
-                    <div className='flex items-center justify-start gap-2 px-1'>
+                    <div className='flex items-center justify-start gap-2'>
                         <input
                             type="checkbox"
                             id="whatsAppCheck"
                             name="whatsAppCheck"
                             checked={showWhatsApp}
-                            onChange={(e) => setShowWhatsApp(!showWhatsApp)}
-                            placeholder='whatsAppCheck' className='border px-5 py-2 rounded-md bg-slate-200'
+                            onChange={() => setShowWhatsApp(!showWhatsApp)}
+                            className='border px-5 py-2 rounded-md'
                         />
-                        <label htmlFor="whatsAppCheck" className='font-medium text-sm'>have a different whatsapp number</label>
+                        <label htmlFor="whatsAppCheck" className='font-medium text-sm'>Have a different WhatsApp number</label>
                     </div>
-                    <div className='flex items-center justify-center gap-2'>
-                        <FaLock size={24} />
+                    <div className='flex items-center justify-center gap-2 rounded-md border px-2'>
                         <input
                             type="password"
                             id="password"
@@ -187,33 +150,31 @@ const CreateAccount = () => {
                             value={formData.password}
                             onChange={handleChange}
                             placeholder='Password'
-                            className='border px-5 py-2 rounded-md bg-slate-200 w-full'
+                            className='w-full px-3 py-3 focus:outline-none'
                         />
                     </div>
-                    <div className={`flex items-center justify-center gap-2 ${!passwordsMatch() && 'border-red-600 focus:border-red-500'}`}>
-                        <FaLock size={24} />
+                    <div className={`flex items-center justify-center gap-2 rounded-md border px-2 ${!passwordsMatch() && formData.confirmPassword ? 'border-red-600 focus:border-red-500' : ''}`}>
                         <input
                             type="password"
                             id="confirmPassword"
                             name="confirmPassword"
-                            value={confirmPass}
-                            onChange={isMatchCheck}
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
                             placeholder='Confirm Password'
-                            className='border px-5 py-2 rounded-md bg-slate-200 w-full'
+                            className='w-full px-3 py-3 focus:outline-none'
                         />
                     </div>
-                    <button type="submit" className='bg-td-secondary h-12  text-white px-5 py-3 rounded-md mt-4 w-full'>
-                        {isLoading && (
+                    <button type="submit" className='bg-td-secondary h-12 text-white px-5 py-3 rounded-md mt-4 w-full'>
+                        {isLoading ? (
                             <PulseLoader color="#ffffff" size={15} />
-                        )}
-                        {!isLoading && (
-                            <span onClick={handleSubmit} className="text-[15px] w-full">Create Account</span>
+                        ) : (
+                            <span className="text-[15px] w-full">Create Account</span>
                         )}
                     </button>
-                    <span className='text-center text-sm'>Already have an account?<span className='cursor-pointer text-blue-900 underline' onClick={() => router.push('/account/login')}>Login</span></span>
+                    <span className='text-center text-sm'>Already have an account? <span className='cursor-pointer text-blue-900 underline' onClick={() => router.push('/account/login')}>Login</span></span>
                 </form>
-            </div >
-        </div >
+            </div>
+        </div>
     );
 };
 
--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import toast from 'react-hot-toast';
-import {  FaLock, FaUser } from 'react-icons/fa';
+import { FaLock, FaUser } from 'react-icons/fa';
 import { PulseLoader } from 'react-spinners';
 
 const LoginPageAdmin = () => {
@@ -52,36 +52,32 @@ const LoginPageAdmin = () => {
   };
 
   return (
-    <div className='bg-td-secondary py-10 flex items-center justify-center px-5 min-h-[70vh]'>
+    <div className='py-10 flex items-center justify-center px-5 min-h-[92vh]'>
       <div className='flex bg-white flex-col gap-5 items-center justify-center w-full md:w-[400px] md:px-10 py-10 px-5 rounded-md'>
         <div>
           <h1 className='font-bold text-[30px] text-td-secondary'>Login</h1>
         </div>
         <div className='flex flex-col gap-3 w-full'>
 
-          <div className='flex items-center w-full justify-center gap-3'>
-            <FaUser size={24} />
-            <div className='flex items-center justify-center rounded-md bg-slate-200 w-full'>
-              <span className='pl-2'>+91</span>
-              <input
-                type="string"
-                // pattern='0-9'
-                id="phone"
-                name="phone"
-                maxLength={10}
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                 placeholder='Phone'
-                className='border px-5  w-full py-2 rounded-md bg-slate-200' />
-            </div>
+          <div className='flex items-center w-full justify-center gap-3 rounded-md border px-2'>
+            <span className=''>+91</span>
+            <input
+              type="string"
+              // pattern='0-9'
+              id="phone"
+              name="phone"
+              maxLength={10}
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder='Phone'
+              className='w-full px-3 py-3 focus:outline-none' />
           </div>
 
-          <div className='flex items-center justify-center gap-2'>
-            <FaLock size={24} />
+          <div className='flex items-center justify-center gap-2 rounded-md border px-2'>
             <input
               type="password"
               placeholder='Password'
-              className='border px-5 py-2 rounded-md bg-slate-200 w-full'
+              className='w-full  px-3 py-3  focus:outline-none'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
