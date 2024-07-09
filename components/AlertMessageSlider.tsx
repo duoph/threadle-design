8bfd02839dc05d149a8da0305021914d61a16826
hadi-razal <hadhirasal22@gmail.com>
2024-07-09 18:21:07 +0530

    udpates in marque

--- components/AlertMessageSlider.tsx
+++ components/AlertMessageSlider.tsx
@@ -5,7 +5,7 @@ const AlertMessageSlider = () => {
     return (
         <div className='py-1 w-full bg-td-primary'>
             <Marquee loop={0} pauseOnHover={true}>
-                <h1 className='text-slate-700 font-extralight'>
+                <h1 className='text-white font-extralight'>
                     This website is still under development. The products listed are demo products . Follow us on Instagram  <a target='_blank' href="https://www.instagram.com/threadle_designs/" className="underline"> @threadle_designs</a>
                 </h1>
             </Marquee>
