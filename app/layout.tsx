11dcd06bcf969b81ea888e298de75c50c70617cc
hadi-razal <hadhirasal22@gmail.com>
2024-07-11 10:02:47 +0530

    meta data update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,16 @@ import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 import { UserProvider } from "@/context/useUser";
 import ProgressBar from "@/components/ProgressBar";
-import AlertMessageSlider from "../components/AlertMessageSlider";
 
 
 
 const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
-  title: "Threadle Designs",
-  keywords: "Threadle Designs, bespoke tailoring, ready-made fashion, modern women's fashion, customization, curated collections, unique garments, ready-to-wear fashion, online boutique, fashion store, Malappuram, Kerala, best online selling store, women's clothing, fashion trends, style and convenience, tailored ensembles, fashion-forward designs, online shopping experience, quality craftsmanship, fashion enthusiasts, trendsetting styles",
-  description: "At Threadle Designs, we blend the artistry of bespoke tailoring with the convenience of ready-made fashion to cater to the diverse needs of modern women. Our boutique offers a seamless fusion of customization and curated collections, ensuring every customer finds their perfect ensemble, whether they seek a uniquely crafted garment or a ready-to-wear masterpiece.",
+  title: "Threadle Designs | Bespoke Tailoring & Ready-to-Wear Fashion in Malappuram, Kerala",
+  keywords: "Threadle Designs, bespoke tailoring, ready-made fashion, women's fashion, custom clothing, curated collections, unique garments, ready-to-wear, online boutique, fashion store, Malappuram, Kerala, online clothing store, women's clothing, fashion trends, tailored outfits, stylish clothing, online shopping, quality craftsmanship, fashion enthusiasts, trendy styles",
+  description: "Discover Threadle Designs in Malappuram, Kerala, where bespoke tailoring meets ready-made fashion. Our boutique offers custom clothing and curated collections, providing modern women with unique, stylish garments and a seamless online shopping experience.",
 };
+
 
 
 export default function RootLayout({
@@ -33,7 +33,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <UserProvider>
           <Header />
-          {/* <AlertMessageSlider/> */}
           <ProgressBar />
           <Toaster
             position="top-center"
