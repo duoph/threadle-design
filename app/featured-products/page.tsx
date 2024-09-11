e31d069116f2bf02febdc5ea6c71e0a822813363
hadi-razal <hadhirasal22@gmail.com>
2024-09-11 18:02:36 +0530

    added max width to the featured product page

--- app/featured-products/page.tsx
+++ app/featured-products/page.tsx
@@ -58,31 +58,33 @@ const WishList = () => {
     }
 
     return (
-        <div className=" lg:px-5 flex flex-col gap-3 min-h-[85vh] py-5">
-            <div className="flex items-center justify-center">
-                <h1 className='text-td-secondary text-center text-[25px] md:text-[35px] font-bold text-3xl'>Featured Products</h1>
-            </div>
-            <div className="flex items-center h-full justify-center  gap-[9px] flex-wrap md:gap-5">
-                {currentProducts.length > 0 && isLoading === false ? (
-                    currentProducts.map((product) => (
-                        product.inStock && <ProductCard getProducts={fetchFeatured} key={product._id} product={product} />
-                    ))
-                ) : (
-                    <div className='flex items-center justify-center h-full w-full'>
-                        <span className=''>No Products Available</span>
-                    </div>
+        <div className=" lg:px-5 flex flex-col items-center justify-center min-h-[85vh] py-5">
+            <div className="flex flex-col items-center justify-center max-w-6xl gap-2">
+                <div className="flex items-center justify-center">
+                    <h1 className='text-td-secondary text-center text-[25px] md:text-[35px] font-bold text-3xl'>Featured Products</h1>
+                </div>
+                <div className="flex items-center h-full justify-center  gap-[9px] flex-wrap md:gap-5">
+                    {currentProducts.length > 0 && isLoading === false ? (
+                        currentProducts.map((product) => (
+                            product.inStock && <ProductCard getProducts={fetchFeatured} key={product._id} product={product} />
+                        ))
+                    ) : (
+                        <div className='flex items-center justify-center h-full w-full'>
+                            <span className=''>No Products Available</span>
+                        </div>
+                    )}
+                </div>
+                {products.length > 20 && (
+                    <ul className="flex items-center justify-center gap-7 h-full">
+                        <li className={`cursor-pointer page-item border flex items-center justify-center text-white rounded-md py-2 bg-td-secondary px-6  ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ' transition-all ease-in-out'}`}>
+                            <button onClick={prevPage} disabled={currentPage === 1} className="flex items-center justify-center "> <MdNavigateBefore size={24} /> <span className='px-2'>Prev</span> </button>
+                        </li>
+                        <li className={`cursor-pointer page-item border flex items-center justify-center text-white rounded-md py-2 bg-td-secondary px-6 ${indexOfLastProduct >= products.length ? 'opacity-50 cursor-not-allowed' : ' transition-all ease-in-out'}`}>
+                            <button onClick={nextPage} disabled={indexOfLastProduct >= products.length} className="flex items-center justify-center"><span className='px-2'>Next</span> <MdNavigateNext size={24} /></button>
+                        </li>
+                    </ul>
                 )}
             </div>
-            {products.length > 20 && (
-                <ul className="flex items-center justify-center gap-7 h-full">
-                    <li className={`cursor-pointer page-item border flex items-center justify-center text-white rounded-md py-2 bg-td-secondary px-6  ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ' transition-all ease-in-out'}`}>
-                        <button onClick={prevPage} disabled={currentPage === 1} className="flex items-center justify-center "> <MdNavigateBefore size={24} /> <span className='px-2'>Prev</span> </button>
-                    </li>
-                    <li className={`cursor-pointer page-item border flex items-center justify-center text-white rounded-md py-2 bg-td-secondary px-6 ${indexOfLastProduct >= products.length ? 'opacity-50 cursor-not-allowed' : ' transition-all ease-in-out'}`}>
-                        <button onClick={nextPage} disabled={indexOfLastProduct >= products.length} className="flex items-center justify-center"><span className='px-2'>Next</span> <MdNavigateNext size={24} /></button>
-                    </li>
-                </ul>
-            )}
         </div>
     );
 };
